34e52fe9f0d644c4ec90dfd6013b1711f4131f2d
theEvilReaper <steffenwx@gmail.com>
2023-03-15 22:40:38 +0100

    Add annotation structure

---
+++ src/main/kotlin/net/theevilreaper/dartpoet/annotation/AnnotationSpec.kt
@@ -0,0 +1,11 @@
+package net.theevilreaper.dartpoet.annotation
+
+
+/**
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since
+ **/
+
+class AnnotationSpec {
+}
---
+++ src/main/kotlin/net/theevilreaper/dartpoet/annotation/AnnotationSpecBuilder.kt
@@ -0,0 +1,15 @@
+package net.theevilreaper.dartpoet.annotation
+
+
+/**
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since
+ **/
+
+class AnnotationSpecBuilder {
+
+    fun build(): AnnotationSpec {
+        return AnnotationSpec()
+    }
+}
