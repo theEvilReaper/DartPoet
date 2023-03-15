20ae2c5951223df51421074a348c8ca3eebee047
theEvilReaper <steffenwx@gmail.com>
2023-03-15 22:41:42 +0100

    Add method to transform a set into a unmodifiable list

--- src/main/kotlin/net/theevilreaper/dartpoet/util/Util.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/util/Util.kt
@@ -1,7 +1,11 @@
 package net.theevilreaper.dartpoet.util
 
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.collections.LinkedHashSet
 
 internal fun <T> Collection<T>.toImmutableSet(): Set<T> =
     Collections.unmodifiableSet(LinkedHashSet(this))
+
+internal fun <T> Collection<T>.toImmutableList(): List<T> =
+    Collections.unmodifiableList(ArrayList(this))
