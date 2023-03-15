0cac233f2b12e76bd7ca6f7ade1e8e1e9765a3b6
theEvilReaper <steffenwx@gmail.com>
2023-03-15 22:42:26 +0100

    Outsource builder into own class

---
+++ src/main/kotlin/net/theevilreaper/dartpoet/property/DartPropertySpec.kt
@@ -0,0 +1,37 @@
+package net.theevilreaper.dartpoet.property
+
+import net.theevilreaper.dartpoet.DartModifier
+import net.theevilreaper.dartpoet.annotation.AnnotationSpec
+import net.theevilreaper.dartpoet.util.toImmutableSet
+import net.theevilreaper.dartpoet.writer.CodeWriter
+
+class DartPropertySpec(
+    builder: DartPropertyBuilder
+) {
+
+    private var name = builder.name
+    private var type = builder.type
+    private var modifiers: Set<DartModifier> = builder.modifiers.toImmutableSet()
+    private var annotations: Set<AnnotationSpec> = builder.annotations.toImmutableSet()
+    private var nullable = builder.nullable
+    private var initBlock = builder.initBlock
+
+    internal fun write(
+        codeWriter: CodeWriter,
+        implicitModifiers: Set<DartModifier>,
+        withInitializer: Boolean = true,
+    ) {
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun builder(
+            name: String,
+            type: String,
+            vararg modifiers: DartModifier
+        ): DartPropertyBuilder {
+            return DartPropertyBuilder(name, type, *modifiers)
+        }
+    }
+}
