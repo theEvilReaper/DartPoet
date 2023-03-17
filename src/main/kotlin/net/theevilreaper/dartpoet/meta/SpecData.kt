bb97c9e09e91dcb9ec0cc084d25b53b262978744
theEvilReaper <steffenwx@gmail.com>
2023-03-17 13:48:29 +0100

    Add SpecData class

---
+++ src/main/kotlin/net/theevilreaper/dartpoet/meta/SpecData.kt
@@ -0,0 +1,74 @@
+package net.theevilreaper.dartpoet.meta
+
+import net.theevilreaper.dartpoet.DartModifier
+import net.theevilreaper.dartpoet.annotation.AnnotationSpec
+
+class SpecData : SpecMethods<Unit> {
+
+    internal val modifiers: MutableList<DartModifier> = mutableListOf()
+    internal val annotations: MutableList<AnnotationSpec> = mutableListOf()
+
+    /**
+     * Add a [Iterable] of [DartModifier].
+     * @param modifiers the modifiers to add
+     */
+    override fun modifiers(modifiers: Iterable<DartModifier>) {
+        this.modifiers += modifiers;
+    }
+
+    /**
+     * Add a [Iterable] of [AnnotationSpec].
+     * @param annotations the annotations to add
+     */
+    override fun annotations(annotations: Iterable<AnnotationSpec>) {
+        this.annotations += annotations
+    }
+
+    /**
+     * Add a [Iterable] of [AnnotationSpec].
+     * @param annotations the annotations to add
+     */
+    override fun annotations(annotations: () -> Iterable<AnnotationSpec>) {
+        this.annotations += annotations()
+    }
+
+    /**
+     * Add a single [AnnotationSpec].
+     * @param annotation the annotation to add
+     */
+    override fun annotation(annotation: () -> AnnotationSpec) {
+        this.annotations += annotation()
+    }
+
+    /**
+     * Add a single [AnnotationSpec].
+     * @param annotation the annotation to add
+     */
+    override fun annotation(annotation: AnnotationSpec) {
+        this.annotations += annotation
+    }
+
+    /**
+     * Add a new [DartModifier].
+     * @param modifier the modifier to add
+     */
+    override fun modifier(modifier: DartModifier) {
+        this.modifiers += modifier
+    }
+
+    /**
+     * Add a new [DartModifier].
+     * @param modifier the modifier to add
+     */
+    override fun modifier(modifier: () -> DartModifier) {
+        this.modifiers += modifier()
+    }
+
+    /**
+     * Add a [Iterable] of [DartModifier].
+     * @param modifiers the modifiers to add
+     */
+    override fun modifiers(modifiers: () -> Iterable<DartModifier>) {
+        this.modifiers += modifiers()
+    }
+}
