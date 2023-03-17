77bedc5302cce01edcdef38cbe818bb91f5eba1c
theEvilReaper <steffenwx@gmail.com>
2023-03-17 22:08:22 +0100

    Add method to create a new instance from the builder and a test method to generation the annotation as code

--- src/main/kotlin/net/theevilreaper/dartpoet/annotation/AnnotationSpec.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/annotation/AnnotationSpec.kt
@@ -1,5 +1,8 @@
 package net.theevilreaper.dartpoet.annotation
 
+import net.theevilreaper.dartpoet.code.CodeFragment
+import net.theevilreaper.dartpoet.util.ANNOTATION_CHAR
+import net.theevilreaper.dartpoet.util.toImmutableSet
 
 /**
  * @author theEvilReaper
@@ -7,5 +10,32 @@ package net.theevilreaper.dartpoet.annotation
  * @since
  **/
 
-class AnnotationSpec {
+class AnnotationSpec(
+    builder: AnnotationSpecBuilder
+) {
+
+    private val name: String = builder.name
+    private val content: Set<CodeFragment> = builder.content.toImmutableSet()
+
+    //TODO: Update later to the class which writes the code. For now it is used to test the generation
+    fun write(): String {
+        val builder = StringBuilder()
+        builder.append("$ANNOTATION_CHAR $name")
+
+        if (content.isEmpty()) {
+            return builder.toString()
+        }
+
+        builder.append("(")
+        //TODO: Write content parts
+        builder.append(")")
+
+        return builder.toString()
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun builder(name: String) = AnnotationSpecBuilder(name)
+    }
 }
