eb33c708ea97cd51914aa366cd21290b75e7a4ae
theEvilReaper <steffenwx@gmail.com>
2023-03-17 11:52:16 +0100

    Update ImportTest

--- src/test/kotlin/net/theevilreaper/dartpoet/DartImportTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/DartImportTest.kt
@@ -2,12 +2,14 @@ package net.theevilreaper.dartpoet
 
 import junit.framework.TestCase.assertEquals
 import net.theevilreaper.dartpoet.import.DartImport
+import net.theevilreaper.dartpoet.import.ImportCastType
 import org.junit.Test
 
 class DartImportTest {
 
-    private val packageImport = "'package:flutter/material.dart';"
-    private val modelImport = "'../../model/item_model.dart' as item;";
+    private val packageImport = "import 'package:flutter/material.dart';"
+    private val modelImport = "import '../../model/item_model.dart' as item;"
+    private val lazyCastImport = "import '../../model/item_model.dart' deferred as item;"
 
     @Test
     fun `test package import`() {
@@ -17,8 +19,13 @@ class DartImportTest {
 
     @Test
     fun `test package import with cast`() {
-        val import = DartImport("../../model/item_model.dart", "item")
-        println(import.toString())
+        val import = DartImport("../../model/item_model.dart", ImportCastType.AS,"item")
         assertEquals(modelImport, import.toString())
+    }
+
+    @Test
+    fun `test lazy package import with cast`() {
+        val import = DartImport("../../model/item_model.dart", ImportCastType.DEFERRED,"item")
+        assertEquals(lazyCastImport, import.toString())
     }
 }
