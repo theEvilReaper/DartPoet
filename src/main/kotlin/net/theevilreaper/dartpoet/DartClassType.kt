b90c360c655bbe94de843bfd46315a8fa29a521a
theEvilReaper <steffenwx@gmail.com>
2023-03-17 20:51:38 +0100

    Add array which contains default modifier

--- src/main/kotlin/net/theevilreaper/dartpoet/DartClassType.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/DartClassType.kt
@@ -1,8 +1,10 @@
 package net.theevilreaper.dartpoet
 
 enum class DartClassType(
-    val keyWord: String
+    val keyWord: String,
+    val defaultModifier: Array<DartModifier>
 ) {
-    CLASS("class"),
-    MIXIN("mixin"),
+    CLASS("class", arrayOf()),
+    ENUM("enum", arrayOf()),
+    MIXIN("mixin", arrayOf()),
 }
