8675d2824c8f4dd0b2573d9556c7335bf4f099cb
theEvilReaper <steffenwx@gmail.com>
2023-03-19 14:07:12 +0100

    Rename class to DartFileBuilder and add methods to set the indent

--- src/main/kotlin/net/theevilreaper/dartpoet/DartFileBuilder.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/DartFileBuilder.kt
@@ -3,13 +3,27 @@ package net.theevilreaper.dartpoet
 import net.theevilreaper.dartpoet.annotation.AnnotationSpec
 import net.theevilreaper.dartpoet.meta.SpecData
 import net.theevilreaper.dartpoet.meta.SpecMethods
+import net.theevilreaper.dartpoet.util.DEFAULT_INDENT
+import java.lang.IllegalArgumentException
 
-class FileSpecBuilder(
+class DartFileBuilder(
     val name: String
-): SpecMethods<FileSpecBuilder> {
+) : SpecMethods<DartFileBuilder> {
 
     internal val specData = SpecData()
+    internal val documentation: MutableList<String> = mutableListOf()
+    internal var indent = DEFAULT_INDENT
 
+    fun indent(indent: String) = apply {
+        if (indent.trim().isEmpty()) {
+            throw IllegalArgumentException("The indent can't be empty")
+        }
+        this.indent = indent
+    }
+
+    fun indent(indent: () -> String) = apply {
+        this.indent(indent())
+    }
 
     override fun annotations(annotations: Iterable<AnnotationSpec>) = apply {
         this.specData.annotations(annotations)
@@ -35,6 +49,10 @@ class FileSpecBuilder(
         this.specData.modifier(modifier)
     }
 
+    override fun modifiers(vararg modifiers: DartModifier) = apply {
+        throw UnsupportedOperationException("Not implemented yet")
+    }
+
     override fun modifiers(modifiers: Iterable<DartModifier>) = apply {
         this.specData.modifiers(modifiers)
     }
@@ -43,7 +61,7 @@ class FileSpecBuilder(
         this.specData.modifiers(modifiers)
     }
 
-    fun build(): FileSpec {
-        return FileSpec()
+    fun build(): DartFile {
+        return DartFile(this)
     }
 }
