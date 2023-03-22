462f587b4c61744f5306b1b7d57e51d34deb4d9d
theEvilReaper <steffenwx@gmail.com>
2023-03-22 23:19:37 +0100

    Add methods to add parameters to a dart method

--- src/main/kotlin/net/theevilreaper/dartpoet/method/DartMethodBuilder.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/method/DartMethodBuilder.kt
@@ -1,6 +1,26 @@
 package net.theevilreaper.dartpoet.method
 
+import net.theevilreaper.dartpoet.parameter.DartParameterSpec
+
 class DartMethodBuilder internal constructor(
-    name: String
+    val name: String
 ) {
+
+    internal val parameters: MutableList<DartParameterSpec> = mutableListOf()
+
+    fun parameter(parameter: DartParameterSpec) = apply {
+        this.parameters += parameter
+    }
+
+    fun parameter(parameter: () -> DartParameterSpec) = apply {
+        this.parameters += parameter()
+    }
+
+    fun parameters(parameterSpec: Iterable<DartParameterSpec>) = apply {
+        this.parameters += parameterSpec
+    }
+
+    fun parameters(parameterSpec: () -> Iterable<DartParameterSpec>) = apply {
+        this.parameters += parameterSpec()
+    }
 }
--- src/main/kotlin/net/theevilreaper/dartpoet/method/DartMethodSpec.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/method/DartMethodSpec.kt
@@ -1,8 +1,21 @@
 package net.theevilreaper.dartpoet.method
 
+import net.theevilreaper.dartpoet.parameter.DartParameterSpec
+import net.theevilreaper.dartpoet.util.toImmutableSet
+
 class DartMethodSpec(
     builder: DartMethodBuilder
 ) {
+
+    val name = builder.name
+    val parameters: Set<DartParameterSpec> = builder.parameters.toImmutableSet()
+    val namedParameters: Set<DartParameterSpec> = if (parameters.isEmpty()) {
+        setOf()
+    } else {
+        parameters.filter { it.named }.toSet()
+    }
+
+
 
     companion object {
 
