963b1961a15adfaf47b53f739cae182a8594ef0b
theEvilReaper <steffenwx@gmail.com>
2023-03-26 15:27:40 +0200

    Fix typo in a constant value name

--- src/main/kotlin/net/theevilreaper/dartpoet/util/Constants.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/util/Constants.kt
@@ -6,7 +6,7 @@ internal const val DEFAULT_INDENT = " "
 internal const val EMPTY_STRING = ""
 internal const val NULL_STRING = "null"
 internal const val NEW_LINE = "\n"
-internal const val SEMICONLON = ";"
+internal const val SEMICOLON = ";"
 
 internal const val AS_PART = "as"
 internal const val IMPORT = "import"
