fe483cdb2c98a2f36c46521b309e5c687e9c9c58
theEvilReaper <steffenwx@gmail.com>
2023-03-26 15:51:11 +0200

    Implement logic to the isDartConventionFileName method

--- src/main/kotlin/net/theevilreaper/dartpoet/util/Constants.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/util/Constants.kt
@@ -14,11 +14,17 @@ internal const val ANNOTATION_CHAR = "@"
 internal const val DART_FILE_ENDING = ".dart"
 internal val NEW_LINE_SPLIT: Pattern = Pattern.compile(NEW_LINE)
 
+private val namePattern: Pattern = Pattern.compile("([a-z]+)_+([a-z]+)")
+
 /**
- * Checks if a given file na.
+ * Checks if a filename matches the DartFile conventions for file names (not class names!)
  * @param fileName the file name to check
  * @return true if the name matches otherwise false
  */
 fun isDartConventionFileName(fileName: String): Boolean {
-    return false
+    if (fileName.trim().isEmpty()) return false
+    if (fileName.contains(DART_FILE_ENDING)) {
+        return namePattern.matcher(fileName.replace(DART_FILE_ENDING, EMPTY_STRING)).matches()
+    }
+    return namePattern.matcher(fileName).matches()
 }
