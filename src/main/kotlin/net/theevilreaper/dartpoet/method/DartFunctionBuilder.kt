32d9c606bba0da0dd1a801cb5aae633404c7c0f4
theEvilReaper <steffenwx@gmail.com>
2023-03-27 20:48:52 +0200

    Add SpecData usage back

--- src/main/kotlin/net/theevilreaper/dartpoet/method/DartFunctionBuilder.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/method/DartFunctionBuilder.kt
@@ -1,11 +1,16 @@
 package net.theevilreaper.dartpoet.method
 
+import net.theevilreaper.dartpoet.DartModifier
+import net.theevilreaper.dartpoet.annotation.AnnotationSpec
+import net.theevilreaper.dartpoet.meta.SpecData
+import net.theevilreaper.dartpoet.meta.SpecMethods
 import net.theevilreaper.dartpoet.parameter.DartParameterSpec
 
 class DartFunctionBuilder internal constructor(
     val name: String
-) {
+) : SpecMethods<DartFunctionBuilder> {
 
+    internal val specData: SpecData = SpecData()
     internal val parameters: MutableList<DartParameterSpec> = mutableListOf()
     internal var async: Boolean = false
     internal var returnType: String? = null
@@ -32,6 +37,42 @@ class DartFunctionBuilder internal constructor(
 
     fun parameters(parameterSpec: () -> Iterable<DartParameterSpec>) = apply {
         this.parameters += parameterSpec()
+    }
+
+    override fun annotations(annotations: Iterable<AnnotationSpec>)= apply {
+        this.specData.annotations(annotations)
+    }
+
+    override fun annotations(annotations: () -> Iterable<AnnotationSpec>) = apply {
+        this.specData.annotations(annotations)
+    }
+
+    override fun annotation(annotation: () -> AnnotationSpec) = apply {
+        this.specData.annotation(annotation)
+    }
+
+    override fun annotation(annotation: AnnotationSpec) = apply {
+        this.specData.annotation(annotation)
+    }
+
+    override fun modifier(modifier: DartModifier) = apply {
+        this.specData.modifier(modifier)
+    }
+
+    override fun modifier(modifier: () -> DartModifier) = apply {
+        this.specData.modifier(modifier)
+    }
+
+    override fun modifiers(vararg modifiers: DartModifier) = apply {
+        this.specData.modifiers += modifiers
+    }
+
+    override fun modifiers(modifiers: Iterable<DartModifier>) = apply {
+        this.specData.modifiers(modifiers)
+    }
+
+    override fun modifiers(modifiers: () -> Iterable<DartModifier>) = apply {
+        this.specData.modifiers(modifiers)
     }
 
     fun build(): DartFunctionSpec {
