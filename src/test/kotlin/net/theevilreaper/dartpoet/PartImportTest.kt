2bad3d6d04045e8ce02a7785d6c41c86ddc678d7
theEvilReaper <steffenwx@gmail.com>
2023-03-29 23:50:10 +0200

    Add new tests which checks if the new conditions works correctly

--- src/test/kotlin/net/theevilreaper/dartpoet/PartImportTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/PartImportTest.kt
@@ -1,12 +1,27 @@
 package net.theevilreaper.dartpoet
 
 import net.theevilreaper.dartpoet.import.PartImport
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class PartImportTest {
 
     private val expectedImport = "part 'item_model.freezed.dart';"
+
+    @Test
+    fun `test import with empty path`() {
+        Assertions.assertThrows(
+            IllegalStateException::class.java,
+            { PartImport(" ") },
+            "The path of an Import can't be empty"
+        )
+        Assertions.assertThrows(
+            IllegalStateException::class.java,
+            { PartImport("") },
+            "The path of an Import can't be empty"
+        )
+    }
 
     @Test
     fun `create part import`() {
