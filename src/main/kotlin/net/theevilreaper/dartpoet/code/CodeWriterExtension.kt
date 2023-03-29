a682ec5e45d5f2ea14fa5d824ccaedc0beaa57d1
theEvilReaper <steffenwx@gmail.com>
2023-03-29 19:17:05 +0200

    Add extension class for the CodeWriter

---
+++ src/main/kotlin/net/theevilreaper/dartpoet/code/CodeWriterExtension.kt
@@ -0,0 +1,20 @@
+package net.theevilreaper.dartpoet.code
+
+import net.theevilreaper.dartpoet.util.CURLY_CLOSE
+import net.theevilreaper.dartpoet.util.CURLY_OPEN
+import net.theevilreaper.dartpoet.util.NEW_LINE
+
+internal val SPECIAL_CHARACTERS = " \n·".toCharArray()
+internal val UNSAFE_LINE_START = Regex("\\s*[-+].*")
+
+fun String.withOpenBrackets(): String {
+
+    for (i in length - 1 downTo 0) {
+        if (this[i] == CURLY_OPEN) {
+            return "$this$NEW_LINE"
+        } else if (this[i] == CURLY_CLOSE) {
+            break
+        }
+    }
+    return "$this $CURLY_OPEN"
+}
