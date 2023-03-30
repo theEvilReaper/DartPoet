7e2b5ffed371dd94e8401e77b9fa71146a271bff
theEvilReaper <steffenwx@gmail.com>
2023-03-30 09:20:02 +0200

    Add new test for an annotation with a name which contains no data

--- src/test/kotlin/net/theevilreaper/dartpoet/annotation/AnnotationSpecTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/annotation/AnnotationSpecTest.kt
@@ -1,11 +1,26 @@
 package net.theevilreaper.dartpoet.annotation
 
 import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
 import kotlin.test.assertEquals
 
 class AnnotationSpecTest {
 
     private val expectedAnnotation = "@jsonIgnore"
+
+    @Test
+    fun `test annotation with empty name`() {
+        assertThrows(
+            IllegalStateException::class.java,
+            { AnnotationSpec.builder("").build() },
+            "The name can't be empty"
+        )
+        assertThrows(
+            IllegalStateException::class.java,
+            { AnnotationSpec.builder(" ").build() },
+            "The name can't be empty"
+        )
+    }
 
     @Test
     fun `test simple annotation without content`() {
