2f37a28478f6452bb63ca3c605a43686d05ba9b0
theEvilReaper <steffenwx@gmail.com>
2023-03-30 23:28:40 +0200

    Improve class layout

--- src/main/kotlin/net/theevilreaper/dartpoet/clazz/DartClassSpec.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/clazz/DartClassSpec.kt
@@ -1,7 +1,5 @@
 package net.theevilreaper.dartpoet.clazz
 
-import net.theevilreaper.dartpoet.DartClassType
-
 class DartClassSpec internal constructor(
     builder: DartClassBuilder
 ) {
@@ -9,34 +7,41 @@ class DartClassSpec internal constructor(
     internal val name = builder.name
     internal val classType = builder.classType
 
-
-
-
-
     companion object {
 
         /**
          * Create a new [DartClassBuilder] instance for a normal dart class.
          * @return the created instance
          */
-        @JvmStatic fun builder(name: String): DartClassBuilder = DartClassBuilder(name, DartClassType.CLASS)
+        @JvmStatic
+        fun builder(name: String): DartClassBuilder = DartClassBuilder(name, ClassType.CLASS)
 
         /**
          * Create a new [DartClassBuilder] instance for an anonymous dart class.
          * @return the created instance
          */
-        @JvmStatic fun anonymousClassBuilder(): DartClassBuilder = DartClassBuilder( null, DartClassType.CLASS)
+        @JvmStatic
+        fun anonymousClassBuilder(): DartClassBuilder = DartClassBuilder(null, ClassType.CLASS)
 
         /**
          * Create a new [DartClassBuilder] instance for a enum dart class.
          * @return the created instance
          */
-        @JvmStatic fun enumClass(name: String): DartClassBuilder = DartClassBuilder(name, DartClassType.ENUM)
+        @JvmStatic
+        fun enumClass(name: String): DartClassBuilder = DartClassBuilder(name, ClassType.CLASS)
 
         /**
          * Create a new [DartClassBuilder] instance for a mixin dart class.
          * @return the created instance
          */
-        @JvmStatic fun mixinClass(name: String): DartClassBuilder = DartClassBuilder(name, DartClassType.MIXIN)
+        @JvmStatic
+        fun mixinClass(name: String): DartClassBuilder = DartClassBuilder(name, ClassType.MIXIN)
+
+        /**
+         * Create a new [DartClassBuilder] instance for a abstract dart class.
+         * @return the created instance
+         */
+        @JvmStatic
+        fun interfaceClass(name: String): DartClassBuilder = DartClassBuilder(name, ClassType.ABSTRACT)
     }
 }
