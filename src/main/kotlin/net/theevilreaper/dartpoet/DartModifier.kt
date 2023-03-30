e3feba97df913d6de98d098c7839766140d648c2
theEvilReaper <steffenwx@gmail.com>
2023-03-30 09:06:31 +0200

    Add new modifier entries to the enum

--- src/main/kotlin/net/theevilreaper/dartpoet/DartModifier.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/DartModifier.kt
@@ -5,9 +5,21 @@ enum class DartModifier(
     private vararg val modifiers: ModifierTarget
 ) {
 
+    PUBLIC("", ModifierTarget.CLASS, ModifierTarget.PROPERTY, ModifierTarget.FUNCTION),
     PRIVATE("_", ModifierTarget.FUNCTION, ModifierTarget.PROPERTY),
     LATE("late", ModifierTarget.PROPERTY),
-    FINAL("final", ModifierTarget.PROPERTY);
+    FINAL("final", ModifierTarget.PROPERTY),
+
+    ASYNC("async", ModifierTarget.FUNCTION),
+    CONST("const", ModifierTarget.PROPERTY),
+
+    ENUM("enum", ModifierTarget.CLASS),
+    MIXIN("mixin", ModifierTarget.CLASS),
+    ABSTRACT("abstract", ModifierTarget.CLASS),
+    FACTORY("factory", ModifierTarget.FUNCTION),
+    LIBRARY("library", ModifierTarget.CLASS),
+
+    VOID("void", ModifierTarget.INTERFACE);
 
     internal fun containsTarget(modifierTarget: ModifierTarget): Boolean {
         return modifiers.contains(modifierTarget)
