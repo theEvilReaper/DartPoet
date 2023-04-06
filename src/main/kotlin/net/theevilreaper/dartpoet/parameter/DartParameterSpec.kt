071b20daba4ec33ea5516a754663b09a84b2dcd9
theEvilReaper <steffenwx@gmail.com>
2023-04-06 18:31:32 +0200

    Add some value checking and more variables from the builder

--- src/main/kotlin/net/theevilreaper/dartpoet/parameter/DartParameterSpec.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/parameter/DartParameterSpec.kt
@@ -1,14 +1,30 @@
 package net.theevilreaper.dartpoet.parameter
 
+import net.theevilreaper.dartpoet.util.toImmutableSet
+
 class DartParameterSpec internal constructor(
-    private val builder: DartParameterBuilder
+    builder: DartParameterBuilder
 ) {
 
-    val name = builder.name
-    val type = builder.type
-    val isNamed = builder.named
+    internal val name = builder.name
+    internal val type = builder.type
+    internal val isNamed = builder.named
+    internal val isNullable = builder.nullable
+    internal val isRequired = builder.required
+    internal val initializer = builder.initializer
+    internal val annotations = builder.specData.annotations.toImmutableSet()
+    internal val modifiers = builder.specData.modifiers.toImmutableSet()
 
+    init {
+        check(name.trim().isNotEmpty()) { "The name of a parameter can't be empty" }
+        check(type.trim().isNotEmpty()) { "The type can't be empty" }
+    }
 
-
-
+    fun toBuilder(): DartParameterBuilder {
+        val builder = DartParameterBuilder(name, type)
+        builder.named = isNamed
+        builder.nullable = isNullable
+        builder.required = isRequired
+        return builder
+    }
 }
