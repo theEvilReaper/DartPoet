47cdd1c646af1bc2c0b0870d7f612e7c9c682da0
theEvilReaper <steffenwx@gmail.com>
2023-04-06 19:07:50 +0200

    Add new test file for the DartClassSpec

---
+++ src/test/kotlin/net/theevilreaper/dartpoet/clazz/DartClassSpecTest.kt
@@ -0,0 +1,44 @@
+package net.theevilreaper.dartpoet.clazz
+
+import net.theevilreaper.dartpoet.DartModifier.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class DartClassSpecTest {
+
+    @Test
+    fun `test wrong abstract class`() {
+        assertThrows(
+            IllegalStateException::class.java,
+            { DartClassSpec.interfaceClass("Test").modifier { ENUM }.build()},
+            "An abstract class can't have [${ABSTRACT.identifier}, ${ENUM.identifier} as modifiers"
+        )
+    }
+
+    @Test
+    fun `test wrong abstract class with mixin`() {
+        assertThrows(
+            IllegalStateException::class.java,
+            { DartClassSpec.interfaceClass("Test").modifier { MIXIN }.build() },
+            "An abstract class can't have [${ABSTRACT.identifier}, ${ENUM.identifier} as modifiers"
+        )
+    }
+
+    @Test
+    fun `test wrong enum class`() {
+        assertThrows(
+            IllegalStateException::class.java,
+            { DartClassSpec.enumClass("Test").modifier { ABSTRACT }.build()},
+            "An enum class can't have [${ABSTRACT.identifier}, ${MIXIN.identifier} as modifiers"
+        )
+    }
+
+    @Test
+    fun `test wrong enum class with mixin`() {
+        assertThrows(
+            IllegalStateException::class.java,
+            { DartClassSpec.enumClass("Test").modifier { MIXIN }.build()},
+            "An enum class can't have [${ABSTRACT.identifier}, ${MIXIN.identifier} as modifiers"
+        )
+    }
+}
