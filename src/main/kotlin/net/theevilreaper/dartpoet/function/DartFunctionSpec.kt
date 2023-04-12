63f6b2635fa3ed5a0ab754b6a6696b14ee5ee7d9
theEvilReaper <steffenwx@gmail.com>
2023-04-12 21:42:49 +0200

    Change visibility from some variables and add more value checks

--- src/main/kotlin/net/theevilreaper/dartpoet/function/DartFunctionSpec.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/function/DartFunctionSpec.kt
@@ -3,6 +3,9 @@ package net.theevilreaper.dartpoet.function
 import net.theevilreaper.dartpoet.DartModifier
 import net.theevilreaper.dartpoet.annotation.AnnotationSpec
 import net.theevilreaper.dartpoet.code.CodeBlock
+import net.theevilreaper.dartpoet.code.CodeWriter
+import net.theevilreaper.dartpoet.code.FunctionWriter
+import net.theevilreaper.dartpoet.code.buildCodeString
 import net.theevilreaper.dartpoet.parameter.DartParameterSpec
 import net.theevilreaper.dartpoet.util.toImmutableSet
 
@@ -10,12 +13,17 @@ class DartFunctionSpec(
     builder: DartFunctionBuilder
 ) {
 
-    private val name = builder.name
-    private val body: CodeBlock = builder.body.build()
-    private val parameters: Set<DartParameterSpec> = builder.parameters.toImmutableSet()
-    private val isAsync: Boolean = builder.async
-    private val annotation: Set<AnnotationSpec> = builder.specData.annotations.toImmutableSet()
+    internal val name = builder.name
+    internal val returnType: String? = builder.returnType
+    internal val body: CodeBlock = builder.body.build()
+    internal val parameters: Set<DartParameterSpec> = builder.parameters.toImmutableSet()
+    internal val isAsync: Boolean = builder.async
+    internal val annotation: Set<AnnotationSpec> = builder.specData.annotations.toImmutableSet()
     private var modifiers: Set<DartModifier> = builder.specData.modifiers.toImmutableSet()
+    internal val isNullable: Boolean = builder.nullable
+
+    internal val isPrivate = modifiers.contains(DartModifier.PRIVATE)
+
     private val namedParameters: Set<DartParameterSpec> = if (parameters.isEmpty()) {
         setOf()
     } else {
@@ -23,9 +31,23 @@ class DartFunctionSpec(
     }
 
     init {
-        check(name.trim().isNotEmpty()) { "The name of a function can't be empty" }
+        require(name.trim().isNotEmpty()) { "The name of a function can't be empty" }
         require(body.isEmpty() || !modifiers.contains(DartModifier.ABSTRACT)) { "An abstract method can't have a body" }
+      //  require (returnType == null && isNullable) { "A void function can't be nullable" }
     }
+
+    internal fun write(
+        codeWriter: CodeWriter
+    ) {
+        FunctionWriter().emit(this, codeWriter)
+    }
+
+    override fun toString() = buildCodeString {
+        write(
+            this,
+        )
+    }
+
 
     companion object {
 
