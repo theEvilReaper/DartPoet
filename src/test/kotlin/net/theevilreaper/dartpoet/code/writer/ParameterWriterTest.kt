e50658502dbc598c767f8b3f3103fe51bddb5e97
theEvilReaper <steffenwx@gmail.com>
2023-04-16 17:32:02 +0200

    Add writer for classes and parameters

---
+++ src/test/kotlin/net/theevilreaper/dartpoet/code/writer/ParameterWriterTest.kt
@@ -0,0 +1,21 @@
+package net.theevilreaper.dartpoet.code.writer
+
+import com.google.common.truth.Truth.assertThat
+import net.theevilreaper.dartpoet.parameter.DartParameterSpec
+import org.junit.jupiter.api.Test
+
+class ParameterWriterTest {
+
+    @Test
+    fun `write int parameter`() {
+        val param = DartParameterSpec.builder("age", "int").build()
+        assertThat(param.toString()).isEqualTo("int age ")
+    }
+
+    @Test
+    fun `write int parameter with initializer`() {
+        val param = DartParameterSpec.builder("age", "int").initializer("%L", "10").build()
+        assertThat(param.toString()).isEqualTo("int age = 10 ")
+
+    }
+}
