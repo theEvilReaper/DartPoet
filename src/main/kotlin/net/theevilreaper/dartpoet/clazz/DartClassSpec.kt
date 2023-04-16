8d8569c204ef962449e6d64f47db2fb7d40a6fdc
theEvilReaper <steffenwx@gmail.com>
2023-04-16 17:29:48 +0200

    Disable some checks

--- src/main/kotlin/net/theevilreaper/dartpoet/clazz/DartClassSpec.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/clazz/DartClassSpec.kt
@@ -1,7 +1,10 @@
 package net.theevilreaper.dartpoet.clazz
 
+import net.theevilreaper.dartpoet.DartModifier
 import net.theevilreaper.dartpoet.DartModifier.*
-import net.theevilreaper.dartpoet.util.containsAnyOf
+import net.theevilreaper.dartpoet.code.CodeWriter
+import net.theevilreaper.dartpoet.code.buildCodeString
+import net.theevilreaper.dartpoet.code.writer.ClassWriter
 import net.theevilreaper.dartpoet.util.toImmutableSet
 
 class DartClassSpec internal constructor(
@@ -14,19 +17,32 @@ class DartClassSpec internal constructor(
     internal val endsWithNewLine = builder.endWithNewLine
     internal val isEnum = builder.isEnumClass
     internal val isAbstract = builder.isAbstract
+    internal val isMixin = builder.isMixinClass
 
     init {
         if (name != null) {
-            check(name.trim().isEmpty()) { "The name can't be empty"}
+            check(name.trim().isNotEmpty()) { "The name can't be empty"}
         }
 
-        check(isEnum && !this.modifiers.containsAnyOf(ABSTRACT, MIXIN)) {
+        /*check(isEnum && !this.modifiers.containsAnyOf(ABSTRACT, MIXIN)) {
             "An enum class can't have [${ABSTRACT.identifier}, ${MIXIN.identifier} as modifiers"
         }
 
         check (isAbstract && !this.modifiers.containsAnyOf(MIXIN, ENUM)) {
             "An abstract class can't have [${ABSTRACT.identifier}, ${ENUM.identifier} as modifiers"
-        }
+        }*/
+    }
+
+    internal fun write(
+        codeWriter: CodeWriter
+    ) {
+        ClassWriter().write(this, codeWriter)
+    }
+
+    override fun toString() = buildCodeString {
+        write(
+            this
+        )
     }
 
     companion object {
@@ -36,7 +52,7 @@ class DartClassSpec internal constructor(
          * @return the created instance
          */
         @JvmStatic
-        fun builder(name: String): DartClassBuilder = DartClassBuilder(name, ClassType.CLASS)
+        fun builder(name: String): DartClassBuilder = DartClassBuilder(name, ClassType.CLASS, DartModifier.CLASS)
 
         /**
          * Create a new [DartClassBuilder] instance for an anonymous dart class.
@@ -50,20 +66,20 @@ class DartClassSpec internal constructor(
          * @return the created instance
          */
         @JvmStatic
-        fun enumClass(name: String): DartClassBuilder = DartClassBuilder(name, ClassType.CLASS)
+        fun enumClass(name: String): DartClassBuilder = DartClassBuilder(name, ClassType.CLASS, ENUM)
 
         /**
          * Create a new [DartClassBuilder] instance for a mixin dart class.
          * @return the created instance
          */
         @JvmStatic
-        fun mixinClass(name: String): DartClassBuilder = DartClassBuilder(name, ClassType.MIXIN)
+        fun mixinClass(name: String): DartClassBuilder = DartClassBuilder(name, ClassType.CLASS, MIXIN)
 
         /**
          * Create a new [DartClassBuilder] instance for a abstract dart class.
          * @return the created instance
          */
         @JvmStatic
-        fun interfaceClass(name: String): DartClassBuilder = DartClassBuilder(name, ClassType.ABSTRACT)
+        fun abstractClass(name: String): DartClassBuilder = DartClassBuilder(name, ClassType.ABSTRACT, ABSTRACT, CLASS)
     }
 }
