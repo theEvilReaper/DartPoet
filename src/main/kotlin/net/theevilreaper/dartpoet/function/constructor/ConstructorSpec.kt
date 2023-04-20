66cfdda6c8d12dfdb9ad6736dd056cc5bef08f20
theEvilReaper <steffenwx@gmail.com>
2023-04-20 15:55:45 +0200

    Add write and toString method

--- src/main/kotlin/net/theevilreaper/dartpoet/function/constructor/ConstructorSpec.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/function/constructor/ConstructorSpec.kt
@@ -1,5 +1,8 @@
 package net.theevilreaper.dartpoet.function.constructor
 
+import net.theevilreaper.dartpoet.code.CodeWriter
+import net.theevilreaper.dartpoet.code.buildCodeString
+import net.theevilreaper.dartpoet.code.writer.ConstructorWriter
 import net.theevilreaper.dartpoet.function.FunctionType
 import net.theevilreaper.dartpoet.util.toImmutableSet
 
@@ -9,7 +12,7 @@ class ConstructorSpec(
 
     internal val name = builder.name
     internal val named = builder.named
-    internal val isNamed = named.orEmpty().trim().isEmpty()
+    internal val isNamed = named.orEmpty().trim().isNotEmpty()
     internal val isLambda = builder.lambda
     internal val isFactory = builder.factory
     internal val body = builder.body
@@ -18,12 +21,22 @@ class ConstructorSpec(
 
     private val modelParameters = builder.parameters.toImmutableSet()
 
-    internal val parameters = modelParameters.filter { !it.isRequired }.toImmutableSet()
+    internal val parameters = modelParameters.filter { !it.isRequired }.toList()
     internal val requiredParameters = modelParameters.filter { it.isRequired }.toImmutableSet()
 
     internal val namedParameters = modelParameters.filter { it.isNamed }.toImmutableSet()
 
+    internal fun write(
+        codeWriter: CodeWriter
+    ) {
+        ConstructorWriter().emit(this, codeWriter)
+    }
 
+    override fun toString() = buildCodeString {
+        write(
+            this,
+        )
+    }
 
     companion object {
 
