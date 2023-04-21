776ec94cc6a6e90c41c873e1cc33e6aa8052a3bf
theEvilReaper <steffenwx@gmail.com>
2023-04-21 08:22:00 +0200

    Remove old write method

--- src/main/kotlin/net/theevilreaper/dartpoet/annotation/AnnotationSpec.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/annotation/AnnotationSpec.kt
@@ -3,7 +3,6 @@ package net.theevilreaper.dartpoet.annotation
 import net.theevilreaper.dartpoet.code.*
 import net.theevilreaper.dartpoet.code.buildCodeString
 import net.theevilreaper.dartpoet.code.writer.AnnotationWriter
-import net.theevilreaper.dartpoet.util.ANNOTATION_CHAR
 import net.theevilreaper.dartpoet.util.toImmutableSet
 
 /**
@@ -23,22 +22,6 @@ class AnnotationSpec(
 
     init {
         check(name.trim().isNotEmpty()) { "The name can't be empty" }
-    }
-
-    //TODO: Update later to the class which writes the code. For now it is used to test the generation
-    fun write(): String {
-        val builder = StringBuilder()
-        builder.append("$ANNOTATION_CHAR$name")
-
-        if (content.isEmpty()) {
-            return builder.toString()
-        }
-
-        builder.append("(")
-        //TODO: Write content parts
-        builder.append(")")
-
-        return builder.toString()
     }
 
     internal fun write(
