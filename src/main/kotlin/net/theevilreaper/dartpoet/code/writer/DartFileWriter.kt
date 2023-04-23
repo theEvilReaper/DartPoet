17bbaabd65d88d4314b2aa9fb4744a36a0bc588a
theEvilReaper <steffenwx@gmail.com>
2023-04-23 17:31:03 +0200

    Improve import writing

--- src/main/kotlin/net/theevilreaper/dartpoet/code/writer/DartFileWriter.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/code/writer/DartFileWriter.kt
@@ -3,35 +3,37 @@ package net.theevilreaper.dartpoet.code.writer
 import net.theevilreaper.dartpoet.DartFile
 import net.theevilreaper.dartpoet.clazz.DartClassSpec
 import net.theevilreaper.dartpoet.code.CodeWriter
+import net.theevilreaper.dartpoet.code.emitExtensions
+import net.theevilreaper.dartpoet.code.writeImports
 import net.theevilreaper.dartpoet.util.NEW_LINE
 
 class DartFileWriter {
 
     private val classWriter = ClassWriter()
-    private val functionWriter = FunctionWriter()
 
-    fun emit(dartFile: DartFile, codeWriter: CodeWriter) {
-        if (dartFile.imports.isNotEmpty()) {
-            dartFile.imports.forEach {
-                codeWriter.emit(it.toString())
-                codeWriter.emit(NEW_LINE)
-            }
-            codeWriter.emit(NEW_LINE)
+    fun emit(dartFile: DartFile, writer: CodeWriter) {
+        if (dartFile.libImport != null) {
+            writer.emit(dartFile.libImport.toString())
         }
 
-        if (dartFile.partImports.isNotEmpty()) {
-            dartFile.partImports.forEach {
-                codeWriter.emit(it.toString())
-                codeWriter.emit(NEW_LINE)
-            }
-            codeWriter.emit(NEW_LINE)
+        dartFile.imports.writeImports(writer, newLineAtBegin = dartFile.libImport != null) {
+            it.toString()
         }
 
-        if (dartFile.specTypes.isNotEmpty()) {
-            dartFile.specTypes.forEach {
-                classWriter.write(it as DartClassSpec, codeWriter)
+        dartFile.partImports.writeImports(writer, newLineAtBegin = dartFile.imports.isNotEmpty()) {
+            it.toString()
+        }
+
+        writer.emit(NEW_LINE)
+
+        if (dartFile.types.isNotEmpty()) {
+            dartFile.types.forEach {
+                classWriter.write(it as DartClassSpec, writer)
             }
+        }
+
+        dartFile.extensions.emitExtensions(writer) {
+            it.write(writer)
         }
     }
-
 }
