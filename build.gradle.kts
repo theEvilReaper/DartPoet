ae70b37a42e31b37b2db34761e7fdd675732f8cd
TheMeinerLP <p.glanz@madfix.me>
2023-04-23 17:20:29 +0200

    Add maven central publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     application
     id("org.jetbrains.changelog") version "2.0.0"
+    `maven-publish`
+    signing
+    id("org.jetbrains.dokka") version "1.8.10"
 }
 
 group = "net.theevilreaper.dartpoet"
@@ -27,7 +30,7 @@ tasks {
     compileKotlin {
         kotlinOptions {
             jvmTarget = "17"
-            useK2 = false
+            languageVersion = "2.0"
         }
     }
 }
@@ -46,4 +49,62 @@ changelog {
     keepUnreleasedSection.set(true)
     unreleasedTerm.set("[Unreleased]")
     groups.set(listOf("Added", "Changed", "Deprecated", "Removed", "Fixed", "Security"))
+}
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components.findByName("kotlin"))
+            groupId = "dev.themeinerlp"
+            artifactId = "dartPoet"
+            version = rootProject.version.toString()
+
+            pom {
+                name.set("Dart Poet")
+                description.set("A Kotlin API which allows the generation of code for dart")
+                url.set("https://github.com/theEvilReaper/DartPoet")
+                licenses {
+                    license {
+                        name.set("AGPL-3.0")
+                        url.set("https://github.com/theEvilReaper/DartPoet/blob/develop/LICENSE")
+                    }
+                }
+                issueManagement {
+                    system.set("Github")
+                    url.set("https://github.com/theEvilReaper/DartPoet/issues")
+                }
+                developers {
+                    developer {
+                        id.set("themeinerlp")
+                        name.set("Phillipp Glanz")
+                        email.set("p.glanz@madfix.me")
+                    }
+                    developer {
+                        id.set("theEvilReaper")
+                        name.set("Steffen Wonning")
+                        email.set("steffenwx@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git@github.com:theEvilReaper/DartPoet.git")
+                    developerConnection.set("scm:git@github.com:theEvilReaper/DartPoet.git")
+                    url.set("https://github.com/theEvilReaper/DartPoet")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = if (version.toString().endsWith("SNAPSHOT")) uri(snapshotsRepoUrl) else uri(releasesRepoUrl)
+            credentials {
+                username = System.getenv("OSSRH_USERNAME")
+                password = System.getenv("OSSRH_TOKEN")
+            }
+        }
+    }
+}
+signing {
+    useInMemoryPgpKeys(System.getenv("SIGNING_KEY"), System.getenv("SIGNING_PASSWORD"))
+    sign(publishing.publications)
 }
