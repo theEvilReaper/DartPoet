aeca21bdf714934c5ed94166d60af8bde88b0e99
theEvilReaper <steffenwx@gmail.com>
2023-05-01 11:10:22 +0200

    Improve isDartConventionFileName tests

--- src/test/kotlin/net/theevilreaper/dartpoet/util/ConstantsTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/util/ConstantsTest.kt
@@ -1,16 +1,33 @@
 package net.theevilreaper.dartpoet.util
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class ConstantsTest {
 
-    @Test
-    fun `test name pattern`() {
-        assertFalse { isDartConventionFileName("") }
-        assertFalse { isDartConventionFileName("Dart_FILE") }
-        assertTrue { isDartConventionFileName("item_model") }
-        assertTrue { isDartConventionFileName("item_model.dart") }
+    companion object {
+
+        @JvmStatic
+        private fun patterns() = Stream.of(
+            Arguments.of("", false),
+            Arguments.of("Dart_FILE", false),
+            Arguments.of("item_model", true),
+            Arguments.of("item_model.dart", true),
+            Arguments.of("model.dart", true)
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("patterns")
+    fun `test name pattern`(name: String, result: Boolean) {
+        if (result) {
+            assertTrue(isDartConventionFileName(name))
+        } else {
+            assertFalse(isDartConventionFileName(name))
+        }
     }
 }
