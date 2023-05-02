44b65530d9a05d2361b7d2a835698aefcfe73f06
theEvilReaper <steffenwx@gmail.com>
2023-05-02 09:30:39 +0200

    Rename variable partImport to libImport

--- src/main/kotlin/net/theevilreaper/dartpoet/import/LibraryImport.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/import/LibraryImport.kt
@@ -1,27 +1,36 @@
 package net.theevilreaper.dartpoet.import
 
-import net.theevilreaper.dartpoet.DartModifier
+import net.theevilreaper.dartpoet.DartModifier.LIBRARY
 import net.theevilreaper.dartpoet.util.SEMICOLON
 
+/**
+ *
+ * @since 1.0.0
+ * @author theEvilReaper
+ */
 class LibraryImport(
     private val path: String,
     private val asPartOf: Boolean = false
 ): Import {
+
+    /**
+     * The init block checks some conditions and throws some exceptions when they conditions are not reached.
+     */
     init {
         check(path.trim().isNotEmpty()) { "The path of an LibraryImport can't be empty" }
     }
 
-    private val partImport: String = buildString {
+    private val libImport: String = buildString {
         if (asPartOf) {
             append("part of ")
         } else {
-            append("${DartModifier.LIBRARY.identifier} ")
+            append("${LIBRARY.identifier} ")
         }
         append(path)
         append(SEMICOLON)
     }
 
-    override fun toString(): String = partImport
+    override fun toString(): String = libImport
 
-    override fun compareTo(other: Import): Int = partImport.compareTo(other.toString())
+    override fun compareTo(other: Import): Int = libImport.compareTo(other.toString())
 }
