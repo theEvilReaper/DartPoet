22d9399919f3e8fce9c80328dab3453fb363d105
theEvilReaper <steffenwx@gmail.com>
2023-05-04 09:03:52 +0200

    Add test for the constructor generation

---
+++ src/test/kotlin/net/theevilreaper/dartpoet/code/writer/ConstructorWriterTest.kt
@@ -0,0 +1,110 @@
+package net.theevilreaper.dartpoet.code.writer
+
+import com.google.common.truth.Truth
+import net.theevilreaper.dartpoet.DartModifier
+import net.theevilreaper.dartpoet.code.CodeBlock
+import net.theevilreaper.dartpoet.function.constructor.ConstructorSpec
+import net.theevilreaper.dartpoet.parameter.DartParameterSpec
+import org.junit.jupiter.api.Test
+
+class ConstructorWriterTest {
+
+    @Test
+    fun `test constructor write without any special parameter`() {
+        val constructor = ConstructorSpec.builder("Car")
+            .parameters {
+                listOf(
+                    DartParameterSpec.builder("maker").build(),
+                    DartParameterSpec.builder("model").build(),
+                    DartParameterSpec.builder("yearMade").build(),
+                    DartParameterSpec.builder("hasABS").build()
+                )
+            }
+            .build()
+        Truth.assertThat(constructor.toString()).isEqualTo(
+            """
+            Car(this.maker, this.model, this.yearMade, this.hasABS);
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun `test named constructor write without any special parameter`() {
+        val constructor = ConstructorSpec.named("Car", "withoutABS")
+            .parameters {
+                listOf(
+                    DartParameterSpec.builder("maker").build(),
+                    DartParameterSpec.builder("model").build(),
+                    DartParameterSpec.builder("yearMade").build(),
+                )
+            }
+            .build()
+        Truth.assertThat(constructor.toString()).isEqualTo(
+            """
+            Car.withoutABS(this.maker, this.model, this.yearMade);
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun `test named constructor2 write without any special parameter`() {
+        val constructor = ConstructorSpec.named("Car", "withoutABS")
+            .parameters {
+                listOf(
+                    DartParameterSpec.builder("maker").build(),
+                    DartParameterSpec.builder("model").build(),
+                    DartParameterSpec.builder("yearMade").build(),
+                )
+            }
+            .addCode(CodeBlock.of(
+                "hasABS = false"
+            ))
+            .build()
+        Truth.assertThat(constructor.toString()).isEqualTo(
+            """
+            Car.withoutABS(this.maker, this.model, this.yearMade): hasABS = false;
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun `test const constructor without any parameter which has special properties`() {
+        val constructor = ConstructorSpec.builder("Car")
+            .modifier(DartModifier.CONST)
+            .parameters {
+                listOf(
+                    DartParameterSpec.builder("maker").build(),
+                    DartParameterSpec.builder("model").build(),
+                    DartParameterSpec.builder("yearMade").build(),
+                )
+            }
+            .build()
+        Truth.assertThat(constructor.toString()).isEqualTo(
+            """
+            const Car(this.maker, this.model, this.yearMade);
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun `test constructor with required and named parameters`() {
+        val constructor = ConstructorSpec.builder("Car")
+            .parameters {
+                listOf(
+                    DartParameterSpec.builder("maker").required(true).build(),
+                    DartParameterSpec.builder("model").named(true).build(),
+                    DartParameterSpec.builder("yearMade").required(true).build(),
+                )
+            }
+            .build()
+        Truth.assertThat(constructor.toString()).isEqualTo(
+            """
+            Car({
+              required this.maker,
+              this.model,
+              required this.yearMade
+            });
+            """.trimIndent()
+        )
+    }
+}
