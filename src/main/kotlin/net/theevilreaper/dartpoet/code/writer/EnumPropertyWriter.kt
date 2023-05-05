9fe94c33b614317560889a7fe9148a259d20e336
theEvilReaper <steffenwx@gmail.com>
2023-05-05 19:16:14 +0200

    Add documentation

--- src/main/kotlin/net/theevilreaper/dartpoet/code/writer/EnumPropertyWriter.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/code/writer/EnumPropertyWriter.kt
@@ -3,7 +3,20 @@ package net.theevilreaper.dartpoet.code.writer
 import net.theevilreaper.dartpoet.code.CodeWriter
 import net.theevilreaper.dartpoet.enum.EnumPropertySpec
 
+/**
+ * The class contains the logic to transform a [EnumPropertySpec] into code.
+ * Each generated part will be applied to a [CodeWriter] reference.
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author theEvilReaper
+ */
 class EnumPropertyWriter {
+
+    /**
+     * Writes the given data from a [EnumPropertySpec] to a [CodeWriter] instance.
+     * @param propertySpec the [EnumPropertySpec] to get the data to write
+     * @param writer the [CodeWriter] instance to apply the code
+     */
     fun write(propertySpec: EnumPropertySpec, writer: CodeWriter) {
         writer.emit(propertySpec.name)
         if (propertySpec.hasGeneric) {
--- src/main/kotlin/net/theevilreaper/dartpoet/enum/EnumPropertySpec.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/enum/EnumPropertySpec.kt
@@ -5,6 +5,11 @@ import net.theevilreaper.dartpoet.code.buildCodeString
 import net.theevilreaper.dartpoet.code.writer.EnumPropertyWriter
 import net.theevilreaper.dartpoet.util.toImmutableList
 
+/**
+ *
+ * @since 1.0.0
+ * @author theEvilReaper
+ */
 class EnumPropertySpec(
     val builder: EnumPropertyBuilder
 ) {
@@ -15,6 +20,10 @@ class EnumPropertySpec(
     internal val parameters = builder.parameters.toImmutableList()
     internal val hasParameter = builder.parameters.isNotEmpty()
 
+    /**
+     * Contains some checks for the variable.
+     * When a variable doesn't pass the check an exception will be thrown.
+     */
     init {
         check(name.trim().isNotEmpty()) { "The name of a EnumProperty can't be empty" }
         if (generic != null && hasGeneric) {
@@ -22,18 +31,27 @@ class EnumPropertySpec(
         }
     }
 
+
     internal fun write(
         codeWriter: CodeWriter
     ) {
         EnumPropertyWriter().write(this, codeWriter)
     }
 
+    /**
+     * Creates a string representation from the spec object.
+     * @return the created string
+     */
     override fun toString() = buildCodeString {
         write(
             this,
         )
     }
 
+    /**
+     * Converts a [EnumPropertySpec] to a [EnumPropertyWriter] instance.
+     * @return the created instance
+     */
     fun toBuilder(): EnumPropertyBuilder {
         val builder = EnumPropertyBuilder(name)
         builder.genericValueCast = generic
@@ -43,6 +61,11 @@ class EnumPropertySpec(
 
     companion object {
 
+        /**
+         * Creates a new instance from the [EnumPropertyBuilder] to construct a new property.
+         * @param name the name for the property
+         * @return the created instance from the [EnumPropertyBuilder]
+         */
         @JvmStatic
         fun builder(name: String) = EnumPropertyBuilder(name)
     }
