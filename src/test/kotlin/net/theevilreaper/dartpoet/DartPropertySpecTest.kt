5a6834bc294783b6f993184ab496d67a4ad08f09
theEvilReaper <steffenwx@gmail.com>
2023-05-15 13:40:43 +0200

    Add more test cases to the property spec and update them to a parameterized test

--- src/test/kotlin/net/theevilreaper/dartpoet/DartPropertySpecTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/DartPropertySpecTest.kt
@@ -2,13 +2,43 @@ package net.theevilreaper.dartpoet
 
 import com.google.common.truth.Truth.assertThat
 import net.theevilreaper.dartpoet.property.DartPropertySpec
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class DartPropertySpecTest {
 
-    @Test
-    fun `test logic`() {
-        val property = DartPropertySpec.builder("test", "String").nullable(true).build()
-        assertThat(property.toString()).isEqualTo("String? test;")
+    companion object {
+
+        @JvmStatic
+        fun parameters() = Stream.of(
+            Arguments.of(
+                DartPropertySpec.builder("test", "String").nullable(true).build(),
+                "String? test;"
+            ),
+            Arguments.of(
+                DartPropertySpec.builder("value", "int").build(),
+                "int value;"
+            ),
+            Arguments.of(
+                DartPropertySpec.builder("data", "int").initWith("%L", "4").build(),
+                "int data = 4;"
+            ),
+            Arguments.of(
+                DartPropertySpec.builder("data", "int").initWith("%L", "4").modifier { DartModifier.FINAL }.build(),
+                "final int data = 4;"
+            ),
+            Arguments.of(
+                DartPropertySpec.builder("id", "String").modifiers { listOf(DartModifier.FINAL, DartModifier.PRIVATE) }.build(),
+                "final String _id;"
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    fun `test properties`(propertySpec: DartPropertySpec, expected: String) {
+        assertThat(propertySpec.toString()).isEqualTo(expected)
     }
 }
