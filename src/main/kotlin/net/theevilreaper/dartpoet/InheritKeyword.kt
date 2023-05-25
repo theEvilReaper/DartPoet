6baad2615c987c52ed38a963ec58371ef2b800ca
theEvilReaper <steffenwx@gmail.com>
2023-05-25 23:51:14 +0200

    Add documentation

--- src/main/kotlin/net/theevilreaper/dartpoet/InheritKeyword.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/InheritKeyword.kt
@@ -1,5 +1,10 @@
 package net.theevilreaper.dartpoet
 
+/**
+ * Contains all available inherit options from the programming language dart.
+ * @author theEvilReaper
+ * @since 1.0.0
+ */
 enum class InheritKeyword(
     val identifier: String
 ) {
