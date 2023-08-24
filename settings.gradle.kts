f5ea3d44c1d57921edef2dcf92dc322b21cc0dab
theEvilReaper <steffenwx@gmail.com>
2023-08-24 13:56:21 +0200

    Move the dependency declaration to the settings.gradle.kts file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "DartPoet"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("google.truth", "com.google.truth", "truth").version("1.1.5")
+            library("junit.api", "org.junit.jupiter", "junit-jupiter-api").version("5.10.0")
+            library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").version("5.10.0")
+            library("junit.params", "org.junit.jupiter", "junit-jupiter-params").version("5.10.0")
+        }
+    }
+}
