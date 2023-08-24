51bd23966df7af86adf7960b527eb13c49361b1e
theEvilReaper <steffenwx@gmail.com>
2023-08-24 14:12:40 +0200

    Move all plugin declarations into the settings.gradle.kts file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,10 @@ dependencyResolutionManagement {
             library("junit.api", "org.junit.jupiter", "junit-jupiter-api").version("5.10.0")
             library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").version("5.10.0")
             library("junit.params", "org.junit.jupiter", "junit-jupiter-params").version("5.10.0")
+
+            plugin("changelog", "org.jetbrains.changelog").version("2.1.2")
+            plugin("dokka", "org.jetbrains.dokka").version("1.8.20")
+            plugin("kotlin.jvm", "org.jetbrains.kotlin.jvm").version("1.9.0")
         }
     }
 }
