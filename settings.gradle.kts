317d4ceadf1fa1410b4e2362082a91ccec78af2e
theEvilReaper <steffenwx@gmail.com>
2023-08-31 09:27:56 +0200

    Add versionRef for the junit dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,11 @@ rootProject.name = "DartPoet"
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
+            version("junit", "5.10.0")
             library("google.truth", "com.google.truth", "truth").version("1.1.5")
-            library("junit.api", "org.junit.jupiter", "junit-jupiter-api").version("5.10.0")
-            library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").version("5.10.0")
-            library("junit.params", "org.junit.jupiter", "junit-jupiter-params").version("5.10.0")
+            library("junit.api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
+            library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
+            library("junit.params", "org.junit.jupiter", "junit-jupiter-params").versionRef("junit")
 
             plugin("changelog", "org.jetbrains.changelog").version("2.1.2")
             plugin("dokka", "org.jetbrains.dokka").version("1.8.20")
