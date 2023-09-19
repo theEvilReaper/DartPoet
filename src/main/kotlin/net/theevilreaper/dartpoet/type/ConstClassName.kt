54afb05146d5e360a95e63a0a069d5624147068c
theEvilReaper <steffenwx@gmail.com>
2023-09-19 18:09:16 +0200

    Add internal keyword to the class

--- src/main/kotlin/net/theevilreaper/dartpoet/type/ConstClassName.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/type/ConstClassName.kt
@@ -2,8 +2,23 @@ package net.theevilreaper.dartpoet.type
 
 import net.theevilreaper.dartpoet.DartModifier
 
-class ConstClassName : ClassName(DartModifier.CONST.identifier) {
+/**
+ * [ConstClassName] represents the `const` keyword from the programming language dart.
+ * It can be used in each spec implementation which allows the usage of [TypeName]'s.
+ *
+ * @constructor Creates an instance of [ConstClassName]
+ * @author theEvilReaper
+ * @since 1.0.0
+ */
+internal class ConstClassName : ClassName(DartModifier.CONST.identifier) {
 
+    /**
+     * This method is overridden from the superclass and throws an [IllegalAccessException] with a message
+     * indicating that the 'const' type cannot be copied.
+     *
+     * @param nullable Indicates whether the type is nullable
+     * @throws IllegalAccessException Always throws an exception indicating that the 'dynamic' type cannot be copied
+     */
     @Throws(IllegalAccessException::class)
     override fun copy(nullable: Boolean): TypeName {
         throw IllegalAccessException("The dynamic type can't be copied")
--- src/main/kotlin/net/theevilreaper/dartpoet/type/DynamicClassName.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/type/DynamicClassName.kt
@@ -6,16 +6,18 @@ import net.theevilreaper.dartpoet.DartModifier
  * [DynamicClassName] extends [ClassName] and is used to represent the 'dynamic' type in Dart programming language.
  * It provides a mechanism to handle the 'dynamic' type, which is not typically used for copying operations.
  *
- * @constructor Creates an instance of [DynamicClassName].
+ * @constructor Creates an instance of [DynamicClassName]
+ * @author theEvilReaper
+ * @since 1.0.0
  */
-class DynamicClassName : ClassName(DartModifier.DYNAMIC.identifier) {
+internal class DynamicClassName : ClassName(DartModifier.DYNAMIC.identifier) {
 
     /**
      * This method is overridden from the superclass and throws an [IllegalAccessException] with a message
      * indicating that the 'dynamic' type cannot be copied.
      *
-     * @param nullable Indicates whether the type is nullable.
-     * @throws IllegalAccessException Always throws an exception indicating that the 'dynamic' type cannot be copied.
+     * @param nullable Indicates whether the type is nullable
+     * @throws IllegalAccessException Always throws an exception indicating that the 'dynamic' type cannot be copied
      */
     @Throws(IllegalAccessException::class)
     override fun copy(nullable: Boolean): TypeName {
