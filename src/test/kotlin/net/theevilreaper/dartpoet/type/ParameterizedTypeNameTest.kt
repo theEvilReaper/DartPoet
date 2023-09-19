2ce80317a99f689327978491683f40d237f4d589
theEvilReaper <steffenwx@gmail.com>
2023-09-19 22:55:12 +0200

    Add more tests for the typename structure

--- src/test/kotlin/net/theevilreaper/dartpoet/type/ParameterizedTypeNameTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/type/ParameterizedTypeNameTest.kt
@@ -2,6 +2,7 @@ package net.theevilreaper.dartpoet.type
 
 import net.theevilreaper.dartpoet.type.ParameterizedTypeName.Companion.parameterizedBy
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -23,11 +24,57 @@ class ParameterizedTypeNameTest {
                 Map::class.parameterizedBy(String::class.asTypeName(), DYNAMIC)
             )
         )
+
+        @JvmStatic
+        private fun typeNameCompanions() = Stream.of(
+            Arguments.of("TestClass<int>", ClassName("TestClass").parameterizedBy(Int::class.asTypeName())),
+            Arguments.of("TestClass<int>", ClassName("TestClass").parameterizedBy(Int::class)),
+            Arguments.of("List<int>", List::class.java.parameterizedBy(Int::class.java)),
+        )
+
+        @JvmStatic
+        private fun testEnclosingTyeName() = Stream.of(
+            Arguments.of(
+                "Map.Entry",
+                ParameterizedTypeName(Map::class.asTypeName(), Map.Entry::class.asClassName(), typeArguments = listOf())
+            ),
+            Arguments.of(
+                "Map.Entry<String, double>",
+                ParameterizedTypeName(
+                    Map::class.asTypeName(),
+                    Map.Entry::class.asClassName(),
+                    typeArguments = listOf(String::class.asTypeName(), Double::class.asTypeName())
+                )
+            )
+        )
     }
 
     @ParameterizedTest
     @MethodSource("typeNameTest")
     fun `test parameterized type name class`(expected: String, parameter: ParameterizedTypeName) {
         assertEquals(expected, parameter.toString())
+    }
+
+    @ParameterizedTest
+    @MethodSource("typeNameCompanions")
+    fun `test method from the parameterized companion object`(expected: String, parameter: ParameterizedTypeName) {
+        assertEquals(expected, parameter.toString())
+    }
+
+    @ParameterizedTest
+    @MethodSource("testEnclosingTyeName")
+    fun `test parameterized write with an enclosingTypeName`(expected: String, parameter: ParameterizedTypeName) {
+        assertEquals(expected, parameter.toString())
+    }
+
+    @Test
+    fun `test copy method from the parameterized TypeName`() {
+        val parameterizedTypeName = List::class.parameterizedBy(Int::class)
+        assertFalse { parameterizedTypeName.isNullable }
+        val nullableType = parameterizedTypeName.copy(nullable = true)
+        assertTrue { nullableType.isNullable }
+        val changedParameter = parameterizedTypeName.copy(nullable = true, listOf(Double::class.asClassName()))
+        assertNotEquals(parameterizedTypeName, changedParameter)
+        assertNotEquals(nullableType, changedParameter)
     }
 }
