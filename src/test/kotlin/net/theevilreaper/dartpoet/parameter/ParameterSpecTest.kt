4aab2fe867a5ed566759ccdd6d4defcbd928ec32
theEvilReaper <steffenwx@gmail.com>
2023-10-09 15:41:47 +0200

    Add parameterized test for invalid parameter creation

--- src/test/kotlin/net/theevilreaper/dartpoet/parameter/ParameterSpecTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/parameter/ParameterSpecTest.kt
@@ -3,9 +3,36 @@ package net.theevilreaper.dartpoet.parameter
 import net.theevilreaper.dartpoet.annotation.AnnotationSpec
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 import kotlin.test.assertContentEquals
 
 class ParameterSpecTest {
+
+    companion object {
+
+        @JvmStatic
+        private fun invalidParameterCreation() = Stream.of(
+            Arguments.of(
+                "The name of a parameter can't be empty",
+                { ParameterSpec.builder("").build() }
+            ),
+            Arguments.of(
+                "A named parameter needs an initializer when it's not nullable",
+                { ParameterSpec.builder("test", String::class).named(true).build() }
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidParameterCreation")
+    fun `test invalid parameter creation`(exceptionMessage: String, parameterSpec: () -> ParameterSpec) {
+        val exception = assertThrows<IllegalArgumentException> { parameterSpec() }
+        assertEquals(exceptionMessage, exception.message)
+    }
 
     @Test
     fun `test spec to builder conversation`() {
