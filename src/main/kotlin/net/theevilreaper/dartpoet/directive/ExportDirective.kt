e09dbbdb7e7b9cf060722329c88f026a95309be9
theEvilReaper <steffenwx@gmail.com>
2024-01-03 16:49:22 +0100

    Implement cast options

--- src/main/kotlin/net/theevilreaper/dartpoet/directive/ExportDirective.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/directive/ExportDirective.kt
@@ -13,18 +13,41 @@ import net.theevilreaper.dartpoet.util.SEMICOLON
  * @author theEvilReaper
  */
 class ExportDirective(
-    private val path: String
+    private val path: String,
+    private val castType: CastType? = null,
+    private val importCast: String? = null,
 ): BaseDirective(path) {
+
+    private val export = "export"
+    private val invalidCastType = arrayOf(CastType.DEFERRED, CastType.AS)
+
+    init {
+        if (castType != null) {
+            if (castType in invalidCastType) {
+                throw IllegalStateException("The following cast types are not allowed for an export directive: [${invalidCastType.joinToString()}]")
+            }
+        }
+
+        if (importCast != null) {
+            check(importCast.trim().isNotEmpty()) { "The importCast can't be empty" }
+        }
+    }
 
     /**
      * Writes an [ExportDirective] with the right syntax to an [CodeWriter] instance.
      * @param writer the [CodeWriter] instance to append the directive
      */
     override fun write(writer: CodeWriter) {
-        writer.emit("export·")
-        writer.emit("'")
-        writer.emit(path.ensureDartFileEnding())
-        writer.emit("'")
-        writer.emit(SEMICOLON)
+        writer.emit("$export·'")
+        if (importCast == null && castType == null) {
+            writer.emit(path.ensureDartFileEnding())
+            writer.emit("'$SEMICOLON")
+        } else if (importCast != null && castType != null) {
+            writer.emit(path.ensureDartFileEnding())
+            writer.emit("' ${castType.identifier} $importCast")
+            writer.emit(SEMICOLON)
+        } else {
+            throw Error("Something went wrong during the ExportDirective write process")
+        }
     }
 }
