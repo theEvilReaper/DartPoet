183dd498ffb42d7d250fd838968bd1c7ffc5e78e
theEvilReaper <steffenwx@gmail.com>
2024-01-10 20:11:17 +0100

    Add test for the multiple cast arguments

--- src/test/kotlin/net/theevilreaper/dartpoet/code/writer/TypeDefWriterTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/code/writer/TypeDefWriterTest.kt
@@ -18,6 +18,7 @@ class TypeDefWriterTest {
     companion object {
 
         private val genericClassName = ClassName("E")
+        private val secondGenericClassName = ClassName("T")
 
         @JvmStatic
         private fun typeDefs(): Stream<Arguments> = Stream.of(
@@ -37,6 +38,40 @@ class TypeDefWriterTest {
                     .returns(Map::class.parameterizedBy(String::class.asTypeName(), DYNAMIC))
                     .build(),
                 "typedef json = Map<String, dynamic>;"
+            ),
+        )
+
+        @JvmStatic
+        private fun multipleCastArguments(): Stream<Arguments> = Stream.of(
+            Arguments.of(
+                TypeDefSpec.builder(
+                    "DoubleValueUpdate",
+                    genericClassName, secondGenericClassName
+                )
+                    .name("Function")
+                    .parameters(
+                        ParameterSpec.builder("first", genericClassName)
+                            .nullable(true)
+                            .build(),
+                        ParameterSpec.builder("second", secondGenericClassName)
+                            .nullable(true)
+                            .build()
+                    )
+                    .build(),
+                "typedef DoubleValueUpdate<E, T> = void Function(E? first, T? second);"
+            ),
+            Arguments.of(
+                TypeDefSpec.builder("Compare", genericClassName, secondGenericClassName)
+                    .returns(Int::class)
+                    .name("Function")
+                    .parameters(
+                        ParameterSpec.builder("a", genericClassName)
+                            .build(),
+                        ParameterSpec.builder("b", genericClassName)
+                            .build()
+                    )
+                    .build(),
+                "typedef Compare<E, T> = int Function(E a, E b);"
             )
         )
     }
@@ -44,6 +79,12 @@ class TypeDefWriterTest {
     @ParameterizedTest
     @MethodSource("typeDefs")
     fun `test typedef write`(typeDef: TypeDefSpec, expected: String) {
+        Truth.assertThat(typeDef.toString()).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @MethodSource("multipleCastArguments")
+    fun `test typedef write with multiple casts`(typeDef: TypeDefSpec, expected: String) {
         Truth.assertThat(typeDef.toString()).isEqualTo(expected)
     }
 }
