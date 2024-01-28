2b85042d3b20172c53b9f14eb474a5b2554c1dcd
theEvilReaper <steffenwx@gmail.com>
2024-01-28 22:51:58 +0100

    Add new test cases and cleanup class

--- src/test/kotlin/net/theevilreaper/dartpoet/util/ConstantsTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/util/ConstantsTest.kt
@@ -1,12 +1,10 @@
 package net.theevilreaper.dartpoet.util
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
-import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
 
 class ConstantsTest {
 
@@ -18,13 +16,11 @@ class ConstantsTest {
             Arguments.of("Dart_FILE", false),
             Arguments.of("item_model", true),
             Arguments.of("item_model.dart", true),
-            Arguments.of("model.dart", true)
-        )
-
-        @JvmStatic
-        private fun camelCaseFormatting() = Stream.of(
-            Arguments.of("testA", formatLowerCamelCase("testA")),
-            Arguments.of("thisIsCamelCase", formatLowerCamelCase("thisisCamelcase"))
+            Arguments.of("model.dart", true),
+            Arguments.of("model_", false),
+            Arguments.of("boss_bar_colour_meep", true),
+            Arguments.of("hello__world.dart", false),
+            Arguments.of("_hello__world_.dart", false),
         )
     }
 
