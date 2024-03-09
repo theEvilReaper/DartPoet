ab1a9f1e7d9aab145e15e4cd98b19715d69e0544
theEvilReaper <steffenwx@gmail.com>
2024-03-09 22:16:08 +0100

    Add test for the toBuilder function

--- src/test/kotlin/net/theevilreaper/dartpoet/function/factory/FactorySpecTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/function/factory/FactorySpecTest.kt
@@ -1,8 +1,13 @@
 package net.theevilreaper.dartpoet.function.factory
 
+import net.theevilreaper.dartpoet.function.ConstructorDelegation
+import net.theevilreaper.dartpoet.parameter.ParameterSpec
 import net.theevilreaper.dartpoet.type.asTypeName
 import org.junit.Assert.assertThrows
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -28,5 +33,22 @@ class FactorySpecTest {
         assertThrows(message, IllegalStateException::class.java) {
             factory.invoke()
         }
+    }
+
+    @Test
+    fun `test to toBuilder conversion`() {
+        val factorySpec = FactorySpec.constBuilder(Int::class.asTypeName())
+            .addCode("()")
+            .delegation(ConstructorDelegation.LAMBDA)
+            .parameter(
+                ParameterSpec.builder("test", Int::class.asTypeName())
+                    .build()
+            )
+            .build()
+        val specAsBuilder = factorySpec.toBuilder()
+        assertEquals(factorySpec.typeName, specAsBuilder.typeName)
+        assertEquals(ConstructorDelegation.LAMBDA, specAsBuilder.delegation)
+        assertTrue(specAsBuilder.const)
+        assertEquals(1, specAsBuilder.parameters.size)
     }
 }
