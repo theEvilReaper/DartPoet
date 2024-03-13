dd1224141ce024c5b800a5c7dd753dc03500600a
theEvilReaper <steffenwx@gmail.com>
2024-03-13 19:54:20 +0100

    Update modifier parsing

--- src/main/kotlin/net/theevilreaper/dartpoet/code/writer/ConstructorWriter.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/code/writer/ConstructorWriter.kt
@@ -7,8 +7,10 @@ import net.theevilreaper.dartpoet.code.Writeable
 import net.theevilreaper.dartpoet.code.emitParameters
 import net.theevilreaper.dartpoet.function.DelegationWriterAdapter
 import net.theevilreaper.dartpoet.function.constructor.ConstructorSpec
+import net.theevilreaper.dartpoet.util.*
 import net.theevilreaper.dartpoet.util.CURLY_CLOSE
 import net.theevilreaper.dartpoet.util.CURLY_OPEN
+import net.theevilreaper.dartpoet.util.EMPTY_STRING
 import net.theevilreaper.dartpoet.util.NEW_LINE
 import net.theevilreaper.dartpoet.util.SEMICOLON
 
@@ -17,9 +19,8 @@ internal class ConstructorWriter : Writeable<ConstructorSpec>, DocumentationAppe
     override fun write(spec: ConstructorSpec, writer: CodeWriter) {
         emitDocumentation(spec.docs, writer)
 
-        if (!spec.isConst && spec.modifiers.contains(DartModifier.CONST)) {
-            writer.emit("${DartModifier.CONST.identifier}·")
-        }
+        val modifiers = getModifiersAsString(spec.isConst, spec.modifiers)
+        writer.emitCode("%L", modifiers)
 
         writer.emit(spec.name)
 
@@ -55,5 +56,16 @@ internal class ConstructorWriter : Writeable<ConstructorSpec>, DocumentationAppe
         writer.emit(")")
 
         DelegationWriterAdapter.appendConstructorDelegation(spec.delegation, spec.initializer.build(), writer)
+    }
+
+    private fun getModifiersAsString(isConst: Boolean, modifiers: Set<DartModifier>): String {
+        return when (isConst) {
+            true -> modifiers.joinToString(separator = SPACE, postfix = SPACE) { it.identifier }
+            else -> if (modifiers.contains(DartModifier.CONST)) {
+                "${DartModifier.CONST.identifier} "
+            } else {
+                EMPTY_STRING
+            }
+        }
     }
 }
