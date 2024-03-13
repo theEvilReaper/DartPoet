8043bdee5d1dd042f4ad112293a17f13b9edc62b
theEvilReaper <steffenwx@gmail.com>
2024-03-13 12:59:24 +0100

    Add documentation

--- src/main/kotlin/net/theevilreaper/dartpoet/function/ConstructorBase.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/function/ConstructorBase.kt
@@ -1,7 +1,12 @@
 package net.theevilreaper.dartpoet.function
 
+import org.jetbrains.annotations.ApiStatus
 
-interface ConstructorBase {
-
-
-}
+/**
+ * The [ConstructorBase] is a marker interface for all classes which are responsible for writing constructors.
+ * @author theEvilReaper
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+@ApiStatus.Internal
+interface ConstructorBase
