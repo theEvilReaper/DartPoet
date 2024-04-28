e5a8e86073667f9e23d06dd19857d358c91503ff
theEvilReaper <steffenwx@gmail.com>
2024-04-28 15:53:29 +0200

    Add constant class for the test package

---
+++ src/test/kotlin/net/theevilreaper/dartpoet/util/TestConstants.kt
@@ -0,0 +1,10 @@
+package net.theevilreaper.dartpoet.util
+
+/**
+ * The file contains some constant values which are used in the test package.
+ * @author theEvilReaper
+ * @since 1.0.0
+ */
+
+const val EMPTY_STRING = ""
+const val SPACE_STRING = " "
