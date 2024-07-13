d008ef817c5f95077bfb85ccc07838cccd9a9674
theEvilReaper <steffenwx@gmail.com>
2024-07-13 20:38:05 +0200

    Add documentation

--- src/main/kotlin/net/theevilreaper/dartpoet/function/MethodAccessorType.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/function/MethodAccessorType.kt
@@ -1,5 +1,14 @@
 package net.theevilreaper.dartpoet.function
 
+/**
+ * The programming language Dart allows different syntaxes to define a method.
+ * Similar to Kotlin, Dart allows the use of setters and getters for properties, and functions can exhibit this behavior in Dart.
+ * This enum defines the different types of accessor methods.
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author theEvilReaper
+ */
 enum class MethodAccessorType {
 
     SETTER,
