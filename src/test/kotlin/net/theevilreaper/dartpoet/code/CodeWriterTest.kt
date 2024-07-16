6b250ee2c87cf6b69ebe335807153f7ab955543f
theEvilReaper <steffenwx@gmail.com>
2024-07-16 10:05:06 +0200

    Add test for the emitSpaces method

---
+++ src/test/kotlin/net/theevilreaper/dartpoet/code/CodeWriterTest.kt
@@ -0,0 +1,19 @@
+package net.theevilreaper.dartpoet.code
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+@DisplayName("Test some functionalities from the CodeWriter class")
+class CodeWriterTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, -1])
+    fun `test invalid space usage`(amount: Int) {
+        assertThrows<IllegalStateException>("The amount of spaces must be greater than 0") {
+            val writer: CodeWriter = CodeWriter(System.out)
+            writer.emitSpaces(amount)
+        }
+    }
+}
