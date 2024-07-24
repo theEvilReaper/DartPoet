503b05e6c1d27de84ff0445a220b6ef482570b85
theEvilReaper <steffenwx@gmail.com>
2024-07-24 13:49:33 +0200

    Update helper class to support TypeDefSpec

--- src/main/kotlin/net/theevilreaper/dartpoet/util/ParameterHelper.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/util/ParameterHelper.kt
@@ -2,13 +2,30 @@ package net.theevilreaper.dartpoet.util
 
 import net.theevilreaper.dartpoet.code.CodeWriter
 import net.theevilreaper.dartpoet.code.emitParameters
+import net.theevilreaper.dartpoet.code.writer.ConstructorWriter
+import net.theevilreaper.dartpoet.code.writer.FunctionWriter
+import net.theevilreaper.dartpoet.code.writer.TypeDefWriter
 import net.theevilreaper.dartpoet.parameter.ParameterSpec
 import net.theevilreaper.dartpoet.util.parameter.ParameterData
 import org.jetbrains.annotations.ApiStatus
 
+/**
+ * The [ParameterHelper] includes a generalization of the write process for each parameter implementation.
+ * It helps to reduce the complexity of the [FunctionWriter], [ConstructorWriter] and [TypeDefWriter] by providing a common way to write.
+ * Before this change, each writer had its own implementation which was redundant and error-prone.
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author theEvilReaper
+ */
 @ApiStatus.Internal
-object ParameterHelper {
+internal object ParameterHelper {
 
+    /**
+     * Excludes the given [ParameterSpec] from the source list.
+     * @param source the source list to exclude the parameters from
+     * @param params the parameters to exclude
+     * @return a new list without the given parameters
+     */
     fun excludeParameters(source: List<ParameterSpec>, vararg params: List<ParameterSpec>): List<ParameterSpec> {
         if (params.isEmpty()) return source.toImmutableList()
 
@@ -16,6 +33,13 @@ object ParameterHelper {
         return source.minus(parametersToRemove).toImmutableList()
     }
 
+    /**
+     * Writes each parameter which is part of the [ParameterData] to the given [CodeWriter].
+     * @param data the data to write
+     * @param codeWriter the writer to append the data
+     * @param indent if the generated code should be indented
+     * @param filterExcluded whether to filter the excluded parameters
+     */
     fun writeParameters(
         data: ParameterData,
         codeWriter: CodeWriter,
@@ -46,6 +70,14 @@ object ParameterHelper {
         codeWriter.emit(")")
     }
 
+    /**
+     * Emits the required and named parameters to the given [CodeWriter].
+     * The mentioned parameters have their own method to reduce the complexity
+     * @param data the data to write
+     * @param codeWriter the writer to append the data
+     * @param indent whether to indent the data
+     * @param filterExcluded whether to filter the excluded parameters
+     */
     private fun emitRequiredAndNamedParameter(
         data: ParameterData,
         codeWriter: CodeWriter,
@@ -60,14 +92,13 @@ object ParameterHelper {
         }
 
         val namedRequired = data.namedParameter.filter { it.isRequired && !it.hasInitializer }.toImmutableList()
-        writeParameters(namedRequired, data.normalParameters.isNotEmpty(), codeWriter)
-        writeParameters(data.requiredParameters, namedRequired.isNotEmpty(), codeWriter)
         val excludedNamedParameters = when (filterExcluded) {
             true -> excludeParameters(data.namedParameter, namedRequired).filter { it.isNullable || it.hasInitializer }
             false -> excludeParameters(data.namedParameter, namedRequired)
         }
-        val emitSpace: Boolean = data.requiredParameters.isNotEmpty() || namedRequired.isNotEmpty()
-        writeParameters(excludedNamedParameters, emitSpace, codeWriter)
+        internalParameterWrite(namedRequired, namedRequired.isNotEmpty(), codeWriter)
+        internalParameterWrite(data.requiredParameters, excludedNamedParameters.isNotEmpty(), codeWriter)
+        internalParameterWrite(excludedNamedParameters, codeWriter = codeWriter)
 
         if (indent) {
             codeWriter.emit(NEW_LINE)
@@ -77,15 +108,22 @@ object ParameterHelper {
         codeWriter.emit("$CURLY_CLOSE")
     }
 
-    private fun writeParameters(
+    /**
+     * Writes each [ParameterSpec] in the list to an active [CodeWriter].
+     * @param parameters the list of parameters to write
+     * @param trailingComma whether to emit a trailing comma
+     * @param codeWriter the active [CodeWriter]
+     */
+    private fun internalParameterWrite(
         parameters: List<ParameterSpec>,
-        emitSpaceComma: Boolean = false,
+        trailingComma: Boolean = false,
         codeWriter: CodeWriter
     ) {
         if (parameters.isEmpty()) return
-        if (emitSpaceComma) {
-            codeWriter.emit(", ")
-        }
+
         parameters.emitParameters(codeWriter, forceNewLines = false)
+        if (trailingComma) {
+            codeWriter.emit(COMMA_SEPARATOR)
+        }
     }
 }
