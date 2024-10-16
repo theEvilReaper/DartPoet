a9d869de082109ae4a92e8faf9397d5d634f73ee
theEvilReaper <steffenwx@gmail.com>
2024-10-16 18:43:46 +0200

    Rename EnumProperty to EnumEntry

--- src/main/kotlin/net/theevilreaper/dartpoet/enum/EnumEntrySpec.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/enum/EnumEntrySpec.kt
@@ -3,17 +3,21 @@ package net.theevilreaper.dartpoet.enum
 import net.theevilreaper.dartpoet.code.CodeWriter
 import net.theevilreaper.dartpoet.code.WriterHelper
 import net.theevilreaper.dartpoet.code.buildCodeString
-import net.theevilreaper.dartpoet.code.writer.EnumPropertyWriter
+import net.theevilreaper.dartpoet.code.writer.EnumEntryWriter
 import net.theevilreaper.dartpoet.util.toImmutableList
 import net.theevilreaper.dartpoet.util.toImmutableSet
+import net.theevilreaper.dartpoet.enum.parameter.EnumParameterSpec
 
 /**
- *
+ * The [EnumEntrySpec] represents a single entry in an enum class.
+ * An entry can have a generic type, parameters and annotations.
+ * Each parameter is wrapped in a [EnumParameterSpec] which contains the data about the parameter.
+ * Dart allows the enum entry can have multiple parameters for an entry.
  * @since 1.0.0
  * @author theEvilReaper
  */
-class EnumPropertySpec internal constructor(
-    val builder: EnumPropertyBuilder
+class EnumEntrySpec internal constructor(
+    val builder: EnumEntryBuilder
 ) {
     internal val name = builder.name
     internal val generic = builder.genericValueCast
@@ -21,7 +25,6 @@ class EnumPropertySpec internal constructor(
     internal val parameters = builder.parameters.toImmutableList()
     internal val hasParameter = builder.parameters.isNotEmpty()
     internal val annotations = builder.annotations.toImmutableSet()
-    internal val useVariableName: Boolean = builder.useVariableName
 
     /**
      * Contains some checks for the variable.
@@ -29,14 +32,10 @@ class EnumPropertySpec internal constructor(
      */
     init {
         check(name.trim().isNotEmpty()) { "The name of a EnumProperty can't be empty" }
-
-        if (useVariableName && parameters.isEmpty()) {
-            throw IllegalStateException("The option 'useVariableName' can't be used without any parameters")
-        }
     }
 
     internal fun write(codeWriter: CodeWriter) {
-        WriterHelper.enumPropertyWriter.write(this, codeWriter)
+        WriterHelper.enumEntryWriter.write(this, codeWriter)
     }
 
     /**
@@ -46,31 +45,28 @@ class EnumPropertySpec internal constructor(
     override fun toString() = buildCodeString { write(this) }
 
     /**
-     * Converts a [EnumPropertySpec] to a [EnumPropertyWriter] instance.
+     * Converts a [EnumEntrySpec] to a [EnumEntryWriter] instance.
      * @return the created instance
      */
-    fun toBuilder(): EnumPropertyBuilder {
-        val builder = EnumPropertyBuilder(this.name)
+    fun toBuilder(): EnumEntryBuilder {
+        val builder = EnumEntryBuilder(this.name)
         builder.annotations.addAll(this.annotations)
         builder.genericValueCast = this.generic
         builder.parameters.addAll(this.parameters)
-        if (useVariableName) {
-            builder.useVariableName()
-        }
         return builder
     }
 
     /**
-     * The companion object contains some helper methods to create a new instance of a [EnumPropertyBuilder].
+     * The companion object contains some helper methods to create a new instance of a [EnumEntryBuilder].
      */
     companion object {
 
         /**
-         * Creates a new instance from the [EnumPropertyBuilder] to construct a new property.
+         * Creates a new instance from the [EnumEntryBuilder] to construct a new property.
          * @param name the name for the property
-         * @return the created instance from the [EnumPropertyBuilder]
+         * @return the created instance from the [EnumEntryBuilder]
          */
         @JvmStatic
-        fun builder(name: String) = EnumPropertyBuilder(name)
+        fun builder(name: String) = EnumEntryBuilder(name)
     }
 }
