5f52d7bd582e0ef8594c3fabb54573bf015e6d4b
theEvilReaper <steffenwx@gmail.com>
2024-12-29 16:20:56 +0100

    Add documentation

--- src/main/kotlin/net/theevilreaper/dartpoet/parameter/ParameterChecker.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/parameter/ParameterChecker.kt
@@ -1,12 +1,20 @@
 package net.theevilreaper.dartpoet.parameter
 
-import org.jetbrains.annotations.ApiStatus.Experimental
 import org.jetbrains.annotations.ApiStatus.Internal
 
-@Experimental
+/**
+ * The [ParameterChecker] provides a set of functions to check the validity of parameters.
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author theEvilReaper
+ */
 @Internal
 internal object ParameterChecker {
 
+    /**
+     * Checks if the given parameters are valid for a function.
+     * @param parameters the parameters to check
+     */
     fun checkRequiredPositional(parameters: List<ParameterSpec>) {
         if (parameters.isEmpty()) return
         parameters.forEach {
@@ -16,6 +24,10 @@ internal object ParameterChecker {
         }
     }
 
+    /**
+     * Checks if the given parameters are valid for a function.
+     * @param parameters the parameters to check
+     */
     fun checkOptionalParameters(parameters: List<ParameterSpec>) {
         if (parameters.isEmpty()) return
         parameters.forEach {
