ddf5c339cd1d3147ee0f871a16c6c7edf88469be
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-11-19 21:15:52 +0100

    chore: update signing configuration to target mavenJava publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -133,5 +133,5 @@ signing {
     val signingKey: String? by project
     val signingPassword: String? by project
     useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications)
+    sign(publishing.publications["mavenJava"])
 }
