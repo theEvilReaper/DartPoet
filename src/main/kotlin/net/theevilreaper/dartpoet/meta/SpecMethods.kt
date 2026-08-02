610e6c8a5982ada9e772aa7bae85f0c3d04c3d29
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-08-02 12:34:35 +0200

    chore(spec): update methods interface

--- src/main/kotlin/net/theevilreaper/dartpoet/meta/SpecMethods.kt
+++ src/main/kotlin/net/theevilreaper/dartpoet/meta/SpecMethods.kt
@@ -1,48 +1,9 @@
 package net.theevilreaper.dartpoet.meta
 
-import net.theevilreaper.dartpoet.DartModifier
-import net.theevilreaper.dartpoet.annotation.AnnotationSpec
-
 /**
- * The interface defines some method to add [AnnotationSpec] and [DartModifier] to a class which implements the interface.
+ * The interface combines [AnnotationMethods] and [ModifierMethods] for a class which needs to add
+ * both annotations and modifiers.
  * @author theEvilReaper
  * @since 1.0.0
  */
-internal interface SpecMethods<T> {
-
-    /**
-     * Add a single [AnnotationSpec].
-     * @param annotation the annotation to add
-     */
-    fun annotation(annotation: () -> AnnotationSpec): T
-
-    /**
-     * Add a single [AnnotationSpec].
-     * @param annotation the annotation to add
-     */
-    fun annotation(annotation: AnnotationSpec): T
-
-    /**
-     * Add an array of [AnnotationSpec].
-     * @param annotations the annotations to add
-     */
-    fun annotations(vararg annotations: AnnotationSpec): T
-
-    /**
-     * Add a new [DartModifier].
-     * @param modifier the modifier to add
-     */
-    fun modifier(modifier: DartModifier): T
-
-    /**
-     * Add a new [DartModifier].
-     * @param modifier the modifier to add
-     */
-    fun modifier(modifier: () -> DartModifier): T
-
-    /**
-     * Add a variable number of [DartModifier]'s.
-     * @param modifiers the modifiers to add
-     */
-    fun modifiers(vararg modifiers: DartModifier): T
-}
+internal interface SpecMethods<T> : AnnotationMethods<T>, ModifierMethods<T>
