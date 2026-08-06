098ce6f15c20c1da9c3be37d2c005ac087dd7e2e
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-08-06 09:05:59 +0200

    test(class): add new guard test

--- src/test/kotlin/net/theevilreaper/dartpoet/classTypes/LibraryClassTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/classTypes/LibraryClassTest.kt
@@ -1,15 +1,27 @@
 package net.theevilreaper.dartpoet.classTypes
 
 import com.google.common.truth.Truth.assertThat
+import net.theevilreaper.dartpoet.clazz.ClassBuilder
 import net.theevilreaper.dartpoet.clazz.ClassSpec
+import net.theevilreaper.dartpoet.constructor.ConstructorSpec
+import net.theevilreaper.dartpoet.function.FunctionSpec
+import net.theevilreaper.dartpoet.property.PropertySpec
+import net.theevilreaper.dartpoet.property.consts.ConstantPropertySpec
 import net.theevilreaper.dartpoet.type.ClassName
-import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 @DisplayName("Test the library class type")
 class LibraryClassTest {
+
+    private lateinit var builder: ClassBuilder
+
+    @BeforeEach
+    fun setUp() {
+        builder = ClassSpec.libraryClass("Foo")
+    }
 
     @Test
     fun `test library class with empty name throws`() {
@@ -22,7 +34,7 @@ class LibraryClassTest {
     @Test
     fun `test library class can't extend a super class`() {
         val exception = assertThrows<IllegalStateException> {
-            ClassSpec.libraryClass("Foo").superClass(ClassName("Bar")).build()
+            builder.superClass(ClassName("Bar")).build()
         }
         assertThat(exception.message).isEqualTo("A library class can't extend, mix in or implement other types")
     }
@@ -30,7 +42,7 @@ class LibraryClassTest {
     @Test
     fun `test library class can't implement interfaces`() {
         val exception = assertThrows<IllegalStateException> {
-            ClassSpec.libraryClass("Foo").implements(ClassName("Bar")).build()
+            builder.implements(ClassName("Bar")).build()
         }
         assertThat(exception.message).isEqualTo("A library class can't extend, mix in or implement other types")
     }
@@ -38,19 +50,40 @@ class LibraryClassTest {
     @Test
     fun `test library class can't declare a generic type`() {
         val exception = assertThrows<IllegalStateException> {
-            ClassSpec.libraryClass("Foo").generic(ClassName("E"))
+            builder.generic(ClassName("E"))
         }
         assertThat(exception.message).isEqualTo("A library class can't have generic types")
     }
 
     @Test
-    @Disabled(
-        "Known bug: ClassWriter#writeClassHeader returns early for ClassType.LIBRARY, so neither the " +
-            "'library' keyword nor the class name are ever emitted. Currently renders as \" {}\" instead " +
-            "of \"library Foo {}\". Enable once ClassWriter is fixed."
-    )
-    fun `test library class renders its keyword and name`() {
-        val libraryClass = ClassSpec.libraryClass("Foo").build()
-        assertThat(libraryClass.toString()).isEqualTo("library Foo {}")
+    fun `test library class can't declare properties`() {
+        val exception = assertThrows<IllegalStateException> {
+            builder.property(PropertySpec.builder("id", Int::class).build())
+        }
+        assertThat(exception.message).isEqualTo("A library class can't declare functions, properties, constructors or constants")
+    }
+
+    @Test
+    fun `test library class can't declare functions`() {
+        val exception = assertThrows<IllegalStateException> {
+            builder.function(FunctionSpec.builder("test").build())
+        }
+        assertThat(exception.message).isEqualTo("A library class can't declare functions, properties, constructors or constants")
+    }
+
+    @Test
+    fun `test library class can't declare constructors`() {
+        val exception = assertThrows<IllegalStateException> {
+            builder.constructor(ConstructorSpec.builder("Foo").build())
+        }
+        assertThat(exception.message).isEqualTo("A library class can't declare functions, properties, constructors or constants")
+    }
+
+    @Test
+    fun `test library class can't declare constants`() {
+        val exception = assertThrows<IllegalStateException> {
+            builder.constant(ConstantPropertySpec.classConst("id", Int::class).initWith("%L", 1).build())
+        }
+        assertThat(exception.message).isEqualTo("A library class can't declare functions, properties, constructors or constants")
     }
 }
