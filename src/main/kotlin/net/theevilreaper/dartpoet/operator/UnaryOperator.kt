6793c16bf0256b8363cb2e39232dcc0fbe3045ca
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-08-09 22:22:35 +0200

    feat(operator): add concrete operator enumerations

---
+++ src/main/kotlin/net/theevilreaper/dartpoet/operator/UnaryOperator.kt
@@ -0,0 +1,9 @@
+package net.theevilreaper.dartpoet.operator
+
+enum class UnaryOperator(
+    override val symbol: String
+) : DartOperator {
+    NEGATE("-"),
+    COMPLEMENT("~"),
+    NOT("!");
+}
