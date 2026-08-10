305dc0a0519a59fe166d6556561bdd8f9a11348e
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-08-10 09:53:00 +0200

    feat(writer): add OperatorWriter class

---
+++ src/main/kotlin/net/theevilreaper/dartpoet/code/writer/operator/OperatorWriter.kt
@@ -0,0 +1,14 @@
+package net.theevilreaper.dartpoet.code.writer.operator
+
+import net.theevilreaper.dartpoet.code.CodeWriter
+import net.theevilreaper.dartpoet.code.Writeable
+import net.theevilreaper.dartpoet.operator.DartOperatorSpec
+
+class OperatorWriter : Writeable<DartOperatorSpec> {
+    override fun write(
+        spec: DartOperatorSpec,
+        writer: CodeWriter
+    ) {
+        TODO("Not yet implemented")
+    }
+}
