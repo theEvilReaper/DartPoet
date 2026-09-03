88a540ce5e1d72338baa9453fcff2820cb5899de
Steffen Wonning <6805083+theEvilReaper@users.noreply.github.com>
2026-09-03 02:32:23 +0200

    feat(class): support mixin modifiers (#309)

--- src/test/kotlin/net/theevilreaper/dartpoet/classTypes/ClassModifierTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/classTypes/ClassModifierTest.kt
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import net.theevilreaper.dartpoet.type.ClassName
 import java.util.stream.Stream
 import kotlin.test.assertEquals
 
@@ -44,6 +45,72 @@ class ClassModifierTest {
                         .build()
                 },
                 "A sealed class can't be combined with the abstract modifier because sealed classes are implicitly abstract"
+            ),
+            Arguments.of(
+                {
+                    ClassSpec.mixinClass("Handler")
+                        .modifier { DartModifier.INTERFACE }
+                        .build()
+                },
+                "A mixin can only have the 'base' modifier, but got: [INTERFACE]"
+            ),
+            Arguments.of(
+                {
+                    ClassSpec.mixinClass("Handler")
+                        .modifier { DartModifier.FINAL }
+                        .build()
+                },
+                "A mixin can only have the 'base' modifier, but got: [FINAL]"
+            ),
+            Arguments.of(
+                {
+                    ClassSpec.mixinClass("Handler")
+                        .modifier { DartModifier.SEALED }
+                        .build()
+                },
+                "A mixin can only have the 'base' modifier, but got: [SEALED]"
+            ),
+            Arguments.of(
+                {
+                    ClassSpec.builder("Handler")
+                        .modifiers(DartModifier.MIXIN, DartModifier.INTERFACE)
+                        .build()
+                },
+                "A mixin class can only be combined with 'base' or 'abstract', but got: [INTERFACE]"
+            ),
+            Arguments.of(
+                {
+                    ClassSpec.builder("Handler")
+                        .modifiers(DartModifier.MIXIN, DartModifier.FINAL)
+                        .build()
+                },
+                "A mixin class can only be combined with 'base' or 'abstract', but got: [FINAL]"
+            ),
+            Arguments.of(
+                {
+                    ClassSpec.builder("Handler")
+                        .modifiers(DartModifier.MIXIN, DartModifier.SEALED)
+                        .build()
+                },
+                "A mixin class can only be combined with 'base' or 'abstract', but got: [SEALED]"
+            ),
+            Arguments.of(
+                {
+                    ClassSpec.builder("Handler")
+                        .modifier { DartModifier.MIXIN }
+                        .superClass(ClassName("Parent"))
+                        .build()
+                },
+                "A mixin class can't extend a class in Dart"
+            ),
+            Arguments.of(
+                {
+                    ClassSpec.builder("Handler")
+                        .modifier { DartModifier.MIXIN }
+                        .withMixins(ClassName("Other"))
+                        .build()
+                },
+                "A mixin class can't use Dart's 'with' clause"
             )
         )
 
@@ -79,7 +146,23 @@ class ClassModifierTest {
             ),
             Arguments.of(
                 ClassSpec.mixinClass("Handler").modifier { DartModifier.BASE }.build(),
-                "mixin Handler {}"
+                "base mixin Handler {}"
+            ),
+            Arguments.of(
+                ClassSpec.builder("Handler").modifier { DartModifier.MIXIN }.build(),
+                "mixin class Handler {}"
+            ),
+            Arguments.of(
+                ClassSpec.builder("Handler").modifiers(DartModifier.BASE, DartModifier.MIXIN).build(),
+                "base mixin class Handler {}"
+            ),
+            Arguments.of(
+                ClassSpec.abstractClass("Handler").modifier { DartModifier.MIXIN }.build(),
+                "abstract mixin class Handler {}"
+            ),
+            Arguments.of(
+                ClassSpec.abstractClass("Handler").modifiers(DartModifier.BASE, DartModifier.MIXIN).build(),
+                "abstract base mixin class Handler {}"
             )
         )
     }
