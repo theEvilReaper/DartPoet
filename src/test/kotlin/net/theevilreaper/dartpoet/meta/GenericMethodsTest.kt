8c9b6d8f7176f587e8c2ce09a72b55deda3fb899
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-09-04 10:11:39 +0200

    test(extension): add new generic cases

--- src/test/kotlin/net/theevilreaper/dartpoet/meta/GenericMethodsTest.kt
+++ src/test/kotlin/net/theevilreaper/dartpoet/meta/GenericMethodsTest.kt
@@ -5,6 +5,7 @@ import net.theevilreaper.dartpoet.clazz.ClassBuilder
 import net.theevilreaper.dartpoet.clazz.ClassType
 import net.theevilreaper.dartpoet.enum.EnumBuilder
 import net.theevilreaper.dartpoet.function.FunctionBuilder
+import net.theevilreaper.dartpoet.extension.ExtensionBuilder
 import net.theevilreaper.dartpoet.mixin.MixinBuilder
 import net.theevilreaper.dartpoet.type.ClassName
 import net.theevilreaper.dartpoet.type.TypeVariableName
@@ -24,6 +25,7 @@ class GenericMethodsTest {
             EnumBuilder("TestEnum"),
             MixinBuilder("TestMixin"),
             FunctionBuilder("testFunc")
+            ExtensionBuilder(name = "TestExt", extClass = ClassName("Object"))
         )
     }
 
@@ -32,6 +34,7 @@ class GenericMethodsTest {
         is EnumBuilder -> builder.genericCasts.map { TypeVariableName.renderDeclaration(it) }
         is MixinBuilder -> builder.genericCasts.map { TypeVariableName.renderDeclaration(it) }
         is FunctionBuilder -> builder.genericCasts.map { TypeVariableName.renderDeclaration(it) }
+        is ExtensionBuilder -> builder.genericTypes.map { TypeVariableName.renderDeclaration(it) }
         else -> error("Unknown builder: $builder")
     }
 
